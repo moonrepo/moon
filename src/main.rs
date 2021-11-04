12968f2958303f8946e905d0fcd6dc0aab5c5097
Miles Johnson <mileswjohnson@gmail.com>
2021-11-03 17:57:57 -0700

    Scaffold rust.

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use ferris_says::say; // from the previous step
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+}

3d15bea0b537c30bbdc1ab70535c7c06b5414c91
Miles Johnson <mileswjohnson@gmail.com>
2021-12-28 09:29:19 -0800

    Rename file.

--- crates/project/src/lib.rs
+++ crates/project/src/lib.rs
@@ -1,16 +1,16 @@
 mod constants;
 mod errors;
-mod graph;
 mod project;
+mod project_graph;
 mod task;
 
 pub use constants::ROOT_NODE_ID;
 pub use errors::ProjectError;
 
 // Projects
-pub use graph::ProjectGraph;
 pub use monolith_config::project::{FileGroups, ProjectID, ProjectType};
 pub use project::{Project, ProjectsMap};
+pub use project_graph::ProjectGraph;
 
 // Tasks
 pub use monolith_config::TaskType;

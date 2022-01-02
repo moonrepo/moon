f4e6974f5b55a327fc3cd3795ef5abee5c371a8f
Miles Johnson <mileswjohnson@gmail.com>
2022-01-01 18:36:04 -0800

    Fix glob check.

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -1,4 +1,3 @@
-use glob::Pattern;
 use moon_logger::{color, trace};
 use std::io::Error;
 use std::path::Path;
@@ -46,6 +45,40 @@ pub async fn exec_command_with_output(bin: &str, args: Vec<&str>) -> Result<Stri
         .to_owned())
 }
 
+// This is not very exhaustive and may be inaccurate.
 pub fn is_glob(value: &str) -> bool {
-    Pattern::new(value).is_ok()
+    let single_values = vec!['*', '?', '1'];
+    let paired_values = vec![('{', '}'), ('[', ']')];
+    let mut bytes = value.bytes();
+    let mut is_escaped = |index: usize| bytes.nth(index - 1).unwrap_or(b' ') == b'\\';
+
+    if value.contains("**") {
+        return true;
+    }
+
+    for single in single_values {
+        if !value.contains(single) {
+            continue;
+        }
+
+        if let Some(index) = value.find(single) {
+            if !is_escaped(index) {
+                return true;
+            }
+        }
+    }
+
+    for (open, close) in paired_values {
+        if !value.contains(open) || !value.contains(close) {
+            continue;
+        }
+
+        if let Some(index) = value.find(open) {
+            if !is_escaped(index) {
+                return true;
+            }
+        }
+    }
+
+    false
 }

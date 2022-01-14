2607479efa02a1dce0f11ade91cf0c308ba2097a
Miles Johnson <mileswjohnson@gmail.com>
2022-01-13 18:03:09 -0800

    Fix lints.

--- crates/utils/src/regex.rs
+++ crates/utils/src/regex.rs
@@ -5,14 +5,14 @@ lazy_static! {
     // Capture group for IDs/names/etc
     static ref ID_GROUP: &'static str = "([A-Za-z]{1}[0-9A-Za-z_-]*)";
 
-    pub static ref ID_PATTERN: Regex = Regex::new(&format!("^{}$", ID_GROUP.to_string())).unwrap();
-    pub static ref TARGET_PATTERN: Regex = Regex::new(&format!("^{}:{}$", ID_GROUP.to_string(), ID_GROUP.to_string())).unwrap();
+    pub static ref ID_PATTERN: Regex = Regex::new(&format!("^{}$", *ID_GROUP)).unwrap();
+    pub static ref TARGET_PATTERN: Regex = Regex::new(&format!("^{}:{}$", *ID_GROUP, *ID_GROUP)).unwrap();
 
     // Token function: `@func(arg)`
     static ref TOKEN_GROUP: &'static str = "([0-9A-Za-z_-]+)";
 
-    pub static ref TOKEN_FUNC_PATTERN: Regex = Regex::new(&format!("^@([a-z]+)\\({}\\)$", TOKEN_GROUP.to_string())).unwrap();
-    pub static ref TOKEN_FUNC_ANYWHERE_PATTERN: Regex = Regex::new(&format!("@([a-z]+)\\({}\\)", TOKEN_GROUP.to_string())).unwrap();
+    pub static ref TOKEN_FUNC_PATTERN: Regex = Regex::new(&format!("^@([a-z]+)\\({}\\)$", *TOKEN_GROUP)).unwrap();
+    pub static ref TOKEN_FUNC_ANYWHERE_PATTERN: Regex = Regex::new(&format!("@([a-z]+)\\({}\\)", *TOKEN_GROUP)).unwrap();
     pub static ref TOKEN_VAR_PATTERN: Regex = Regex::new("\\$([a-zA-Z]+)").unwrap();
 }
 

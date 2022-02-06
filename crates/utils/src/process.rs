a5121c301e5a5044e6e6d7014072a7a8872370ec
Miles Johnson <mileswjohnson@gmail.com>
2022-02-06 14:31:53 -0800

    Update logging.

--- crates/utils/src/process.rs
+++ crates/utils/src/process.rs
@@ -1,6 +1,6 @@
 use crate::fs::get_home_dir;
 use moon_error::{map_io_to_process_error, MoonError};
-use moon_logger::{color, trace};
+use moon_logger::{color, logging_enabled, trace};
 use std::ffi::OsStr;
 use tokio::process::Command;
 
@@ -13,34 +13,37 @@ pub fn create_command<S: AsRef<OsStr>>(bin: S) -> Command {
 pub async fn exec_command(command: &mut Command) -> Result<Output, MoonError> {
     let output = command.output(); // Start immediately
 
-    let base_command = command.as_std();
-    let bin_name = base_command.get_program().to_str().unwrap_or("<unknown>");
-    let args_list = base_command
-        .get_args()
-        .into_iter()
-        .map(|a| a.to_str().unwrap())
-        .collect::<Vec<_>>();
-    let command_line = format!("{} {}", bin_name, args_list.join(" "))
-        .replace(get_home_dir().unwrap_or_default().to_str().unwrap(), "~");
+    // Avoid all this overhead if we're not logging
+    if logging_enabled() {
+        let cmd = command.as_std();
+        let bin_name = cmd.get_program().to_str().unwrap_or("<unknown>");
+        let args_list = cmd
+            .get_args()
+            .into_iter()
+            .map(|a| a.to_str().unwrap())
+            .collect::<Vec<_>>();
+        let command_line = format!("{} {}", bin_name, args_list.join(" "))
+            .replace(get_home_dir().unwrap_or_default().to_str().unwrap(), "~");
 
-    if let Some(cwd) = base_command.get_current_dir() {
-        trace!(
-            target: "moon:utils",
-            "Running command {} (in {})",
-            color::shell(&command_line),
-            color::file_path(cwd),
-        );
-    } else {
-        trace!(
-            target: "moon:utils",
-            "Running command {} ",
-            color::shell(&command_line),
-        );
+        if let Some(cwd) = cmd.get_current_dir() {
+            trace!(
+                target: "moon:utils",
+                "Running command {} (in {})",
+                color::shell(&command_line),
+                color::file_path(cwd),
+            );
+        } else {
+            trace!(
+                target: "moon:utils",
+                "Running command {} ",
+                color::shell(&command_line),
+            );
+        }
     }
 
-    Ok(output
-        .await
-        .map_err(|e| map_io_to_process_error(e, bin_name))?)
+    Ok(output.await.map_err(|e| {
+        map_io_to_process_error(e, command.as_std().get_program().to_str().unwrap())
+    })?)
 }
 
 pub async fn exec_command_capture_stdout(command: &mut Command) -> Result<String, MoonError> {

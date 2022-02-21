75097d283f0f26b5ef7f4c2cdeb4ff1807fff961
Miles Johnson <mileswjohnson@gmail.com>
2022-02-20 16:32:24 -0800

    Setup npm packages.

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+
+// We do *not* support 32-bit at this time
+
+const supportedPlatforms: Record<string, string> = {
+	darwin: 'macos',
+	linux: 'linux',
+	win32: 'windows',
+};
+
+const supportedArchs: Record<string, string> = {
+	arm64: 'arm64',
+	x64: 'x64',
+};
+
+const platform = supportedPlatforms[process.platform];
+const arch = supportedArchs[process.arch];
+
+if (!platform) {
+	throw new Error(`Unsupported platform "${process.platform}".`);
+}
+
+if (!arch) {
+	throw new Error(`Unsupported architecture "${process.arch}".`);
+}
+
+const parts = [platform, arch];
+
+if (platform === 'linux') {
+	const { MUSL, familySync } = require('detect-libc');
+
+	if (familySync() === MUSL) {
+		parts.push('musl');
+	} else {
+		parts.push('gnu');
+	}
+} else if (platform === 'windows') {
+	parts.push('msvc');
+}
+
+export default (() => {
+	const name = parts.join('-');
+
+	try {
+		return require(`@moonrepo/core-${name}`);
+	} catch {
+		return require(`./moon.${name}.node`);
+	}
+})();

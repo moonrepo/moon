59829bc77e3a3f1183be4aa4b0346338ea1971ee
Miles Johnson <mileswjohnson@gmail.com>
2022-03-16 16:02:50 -0700

    Add new cache item.

--- crates/cli/src/commands/setup.rs
+++ crates/cli/src/commands/setup.rs
@@ -4,7 +4,7 @@ pub async fn setup() -> Result<(), Box<dyn std::error::Error>> {
     let workspace = Workspace::load().await?;
     let mut root_package = workspace.load_package_json().await?;
 
-    workspace.toolchain.setup(&mut root_package).await?;
+    workspace.toolchain.setup(&mut root_package, true).await?;
 
     Ok(())
 }
--- crates/workspace/src/tasks/setup_toolchain.rs
+++ crates/workspace/src/tasks/setup_toolchain.rs
@@ -4,6 +4,11 @@ use moon_logger::debug;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
+const SECOND: u128 = 1000;
+const MINUTE: u128 = SECOND * 60;
+const HOUR: u128 = MINUTE * 60;
+const DAY: u128 = HOUR * 24;
+
 pub async fn setup_toolchain(workspace: Arc<RwLock<Workspace>>) -> Result<(), WorkspaceError> {
     debug!(
         target: "moon:task-runner:setup-toolchain",
@@ -11,9 +16,26 @@ pub async fn setup_toolchain(workspace: Arc<RwLock<Workspace>>) -> Result<(), Wo
     );
 
     let workspace = workspace.read().await;
+    let mut cache = workspace.cache.cache_workspace_state().await?;
     let mut root_package = workspace.load_package_json().await?;
 
-    workspace.toolchain.setup(&mut root_package).await?;
+    // Only check the versions of some tools every 24 hours,
+    // as checking every run has considerable overhead spawning all
+    // the child processes. Revisit the threshold if need be.
+    let now = cache.now_millis();
+    let check_versions = cache.item.last_version_check_time == 0
+        || (cache.item.last_version_check_time + DAY) <= now;
+
+    workspace
+        .toolchain
+        .setup(&mut root_package, check_versions)
+        .await?;
+
+    // Update the cache with the timestamp
+    if check_versions {
+        cache.item.last_version_check_time = now;
+        cache.save().await?;
+    }
 
     Ok(())
 }

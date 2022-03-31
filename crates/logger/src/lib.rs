ed2598994a09359700aacc2bcc4dc402158684b0
Miles Johnson <mileswjohnson@gmail.com>
2022-03-31 15:33:07 -0700

    Remove ansi term.

--- crates/logger/src/lib.rs
+++ crates/logger/src/lib.rs
@@ -3,7 +3,8 @@ mod logger;
 
 pub use logger::Logger;
 
-// Re-export so that consumers dont need to install the log crate
+// Re-export so that consumers dont need to install these crates
+pub use console::{measure_text_width, pad_str, pad_str_with, strip_ansi_codes};
 pub use log::{debug, error, info, max_level, trace, warn, LevelFilter};
 
 pub fn logging_enabled() -> bool {

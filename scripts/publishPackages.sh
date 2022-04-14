a530b3f0f2bfd2e4412545bcc9cc9d025f52acb3
Miles Johnson <mileswjohnson@gmail.com>
2022-04-13 22:19:36 -0700

    Fix pkg name.

--- scripts/publishPackages.sh
+++ scripts/publishPackages.sh
@@ -19,7 +19,7 @@ fi
 
 # We only want to publish packages relating to the Rust binary.
 # Other packages will be published the classic way.
-for package in packages/core packages/core-*; do
+for package in packages/cli packages/core-*; do
 	if [[ -z "${GITHUB_TOKEN}" ]]; then
 		echo $package # Testing locally
 	else

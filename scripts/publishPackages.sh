3ed7bd464897bfacc8035727425409236a5cc978
Miles Johnson <milesj@users.noreply.github.com>
2022-04-14 17:56:21 -0700

    build: Fix release workflow. (#85)
    * Enable release flow.
    
    * Try installing pkg-config.
    
    * Debug pkg-config.
    
    * Use a string.
    
    * Set PKG_CONFIG_PATH.
    
    * Install ssl.
    
    * Always yes.
    
    * Fix update.
    
    * Try a different image.
    
    * what.
    
    * Debug.
    
    * Try a different image.
    
    * Remove yarn.
    
    * Remove yarn.
    
    * Dono
    
    * Try alpine zig again.
    
    * Try debian again.
    
    * Remove image.
    
    * Combine flows.
    
    * Enable publish.
    
    * Add bash script.
    
    * Polish.
    
    * Comment out git.
    
    * Swap order.
    
    * Try cd.
    
    * Publish with yarn.
    
    * Fix pkg name.
    
    * Add auth token for yarn.
    
    * Uncomment.
    
    * Chmod file.

---
+++ scripts/publishPackages.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Determine release channel. If contains "alpha", "beta", or "rc",
+# then publish to next, otherwise latest.
+tag=latest
+
+if git log -1 --pretty=%B | grep -e "-alpha" -e "-beta" -e "-rc"; then
+	tag=next
+fi
+
+if [[ -z "${NPM_TOKEN}" ]]; then
+	echo "Missing NPM_TOKEN!"
+else
+	echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+	echo "npmAuthToken: $NPM_TOKEN" >> ./.yarnrc.yml
+
+	# Update env var in GitHub actions
+	echo "NPM_CHANNEL=$tag" >> $GITHUB_ENV
+fi
+
+# We only want to publish packages relating to the Rust binary.
+# Other packages will be published the classic way.
+for package in packages/cli packages/core-*; do
+	if [[ -z "${GITHUB_TOKEN}" ]]; then
+		echo $package # Testing locally
+	else
+		cd "./$package" || exit
+		# We cant use npm because of: https://github.com/npm/cli/issues/2610
+		yarn npm publish --tag $tag --access public
+		cd ../..
+	fi
+done

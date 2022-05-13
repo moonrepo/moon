dba5f38aeda7f2b07b9a82df4ff74bc6509176a3
Miles Johnson <mileswjohnson@gmail.com>
2022-05-13 16:36:13 -0700

    Log git.

--- crates/utils/src/test.rs
+++ crates/utils/src/test.rs
@@ -14,16 +14,16 @@ pub fn create_fixtures_sandbox(dir: &str) -> assert_fs::fixture::TempDir {
 
     // Initialize a git repo so that VCS commands work
     Command::new("git")
-        .arg("init")
+        .args(["init", "--initial-branch", "master"])
         .current_dir(temp_dir.path())
-        .output()
+        .spawn()
         .unwrap_or_else(|_| panic!("Failed to initialize git for fixtures sandbox: {}", dir));
 
     // We must also add the files to the index
     Command::new("git")
         .args(["add", "--all", "."])
         .current_dir(temp_dir.path())
-        .output()
+        .spawn()
         .unwrap_or_else(|_| {
             panic!(
                 "Failed to add files to git index for fixtures sandbox: {}",
@@ -35,7 +35,7 @@ pub fn create_fixtures_sandbox(dir: &str) -> assert_fs::fixture::TempDir {
     Command::new("git")
         .args(["commit", "-m", "'Fixtures'"])
         .current_dir(temp_dir.path())
-        .output()
+        .spawn()
         .unwrap_or_else(|_| panic!("Failed to commit files for fixtures sandbox: {}", dir));
 
     temp_dir

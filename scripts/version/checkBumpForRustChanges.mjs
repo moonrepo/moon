4cc55c2ea5ccdc2018be4eaf65bacfb1ac008c1e
Miles Johnson <mileswjohnson@gmail.com>
2022-05-15 18:07:44 -0700

    Update error.

--- scripts/version/checkBumpForRustChanges.mjs
+++ scripts/version/checkBumpForRustChanges.mjs
@@ -1,11 +1,12 @@
 import fs from 'fs';
+import chalk from 'chalk';
 import yaml from 'yaml';
 import { getChangedFiles } from '../git.mjs';
 
 async function run() {
 	const changedFiles = await getChangedFiles();
 	const hasRustChanges = changedFiles.some(
-		(file) => file.startsWith('crates') && file.endsWith('.rs'),
+		(file) => file.startsWith('crates') && file.endsWith('.rs') && !file.endsWith('_test.rs'),
 	);
 
 	// Exit if no changes to Rust code
@@ -26,8 +27,12 @@ async function run() {
 	});
 
 	if (!hasVersionBump) {
-		throw new Error(
-			`Changes to Rust code detected, but no version bump for the CLI package. Run \`yarn version:bump:bin <major|minor|patch>\` to bump.`,
+		process.exitCode = 1;
+
+		console.error(
+			`Detected changes to Rust code but no version bump for the CLI package. Run ${chalk.magenta(
+				'yarn version:bump:bin <major|minor|patch>',
+			)} to bump.`,
 		);
 	}
 }

acdb7e4a5eb08406d18109deb9c3e62e0b86363e
Miles Johnson <mileswjohnson@gmail.com>
2022-05-21 10:50:38 -0700

    Add dir check.

--- crates/cli/src/commands/init.rs
+++ crates/cli/src/commands/init.rs
@@ -2,12 +2,40 @@ use moon_config::constants::{CONFIG_DIRNAME, CONFIG_PROJECT_FILENAME, CONFIG_WOR
 use moon_config::{load_global_project_config_template, load_workspace_config_template};
 use moon_logger::color;
 use moon_utils::fs;
+use moon_utils::path;
 use std::env;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
-pub async fn init(dest: &str, force: bool) -> Result<(), Box<dyn std::error::Error>> {
+use dialoguer::Confirm;
+
+type AnyError = Box<dyn std::error::Error>;
+
+/// Verify the destination and return a path to the `.moon` folder
+/// if all questions have passed.
+fn verify_dest(dest_dir: &Path) -> Result<Option<PathBuf>, AnyError> {
+    if Confirm::new()
+        .with_prompt(format!("Initialize moon into {}?", color::path(dest_dir)))
+        .interact()?
+    {
+        let moon_dir = dest_dir.join(CONFIG_DIRNAME);
+
+        if moon_dir.exists()
+            && !Confirm::new()
+                .with_prompt("Moon has already been initialized, overwrite it?")
+                .interact()?
+        {
+            return Ok(None);
+        }
+
+        return Ok(Some(moon_dir));
+    }
+
+    Ok(None)
+}
+
+pub async fn init(dest: &str, force: bool) -> Result<(), AnyError> {
     let working_dir = env::current_dir().unwrap();
     let dest_path = PathBuf::from(dest);
     let dest_dir = if dest == "." {
@@ -18,17 +46,10 @@ pub async fn init(dest: &str, force: bool) -> Result<(), Box<dyn std::error::Err
         working_dir.join(dest)
     };
 
-    let moon_dir = dest_dir.join(CONFIG_DIRNAME);
-
-    if moon_dir.exists() && !force {
-        println!(
-            "Moon has already been initialized in {} (pass {} to overwrite)",
-            color::path(&dest_dir.canonicalize().unwrap()),
-            color::shell("--force")
-        );
-
-        return Ok(());
-    }
+    let moon_dir = match verify_dest(&path::normalize(&dest_dir))? {
+        Some(dir) => dir,
+        None => return Ok(()),
+    };
 
     // Create config files
     fs::create_dir_all(&moon_dir).await?;

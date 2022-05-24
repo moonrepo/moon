f184fc5b812da2414a5668fa9d8da2d0ffb747d9
Miles Johnson <mileswjohnson@gmail.com>
2022-05-24 14:33:47 -0700

    Update test.

--- crates/cli/tests/run_test.rs
+++ crates/cli/tests/run_test.rs
@@ -116,15 +116,12 @@ mod caching {
             .await
             .unwrap();
 
-        assert_eq!(
-            read_to_string(
-                fixture
-                    .path()
-                    .join(format!(".moon/cache/hashes/{}.json", state.item.hash))
-            )
-            .unwrap(),
-            String::from("")
-        );
+        assert_snapshot!(read_to_string(
+            fixture
+                .path()
+                .join(format!(".moon/cache/hashes/{}.json", state.item.hash))
+        )
+        .unwrap());
 
         assert_eq!(state.item.exit_code, 0);
         assert_eq!(state.item.stdout, "stdout");

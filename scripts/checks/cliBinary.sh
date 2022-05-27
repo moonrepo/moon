29cbb0d974686b4b5da4498971ef16f2e920d7f2
Miles Johnson <mileswjohnson@gmail.com>
2022-05-26 18:57:04 -0700

    Fix script.

--- scripts/checks/cliBinary.sh
+++ scripts/checks/cliBinary.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Ensure the CLI binary has not been accidently modified and pushed
-# size=$(stat -c %s packages/cli/moon)
-size=$(stat -f "%z" packages/cli/moon) # macos
+size=$(stat -c %s packages/cli/moon)
+# size=$(stat -f "%z" packages/cli/moon) # macos
 
 if [[ "$size" -gt "70" ]]; then
 	echo "Binary 'packages/cli/moon' was modified, please revert!"

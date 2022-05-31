302a048208afb30708925e2fb277e6faf3d73cdf
Miles Johnson <mileswjohnson@gmail.com>
2022-05-30 21:20:07 -0700

    Rewrite test binary.

---
+++ scripts/release/testBinary.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+dir=$(dirname $0)
+
+source "$dir/../helpers.sh"
+
+target="$TARGET"
+package=$(getPackageFromTarget "$target")
+binary=$(getBinaryName)
+
+echo "Target: $target"
+echo "Package: $package"
+echo "Binary: $binary"
+
+# Ensure its "linked" in the package
+"$PWD/node_modules/@moonrepo/$package/$binary" --help
+

2abd8738569c335b93e157feb6f94af6f305d41a
Miles Johnson <mileswjohnson@gmail.com>
2022-06-21 11:38:57 -0700

    Disable offline checl.

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -8,7 +8,6 @@ pub mod time;
 
 use cached::proc_macro::cached;
 use std::env;
-use std::time::Duration;
 
 #[macro_export]
 macro_rules! string_vec {
@@ -26,28 +25,32 @@ pub fn is_ci() -> bool {
     env::var("CI").is_ok()
 }
 
+// TODO: This doesn't work behind VPN or corporate proxies. Disabling for now
+// until we can figure out a better solution.
 #[cached(time = 60)]
 pub fn is_offline() -> bool {
-    use std::net::{Shutdown, SocketAddr, TcpStream};
+    false
+    // use std::time::Duration;
+    // use std::net::{Shutdown, SocketAddr, TcpStream};
 
-    let addresses = [
-        // Cloudflare DNS: https://1.1.1.1/dns/
-        SocketAddr::from(([1, 1, 1, 1], 53)),
-        SocketAddr::from(([1, 0, 0, 1], 53)),
-        // Google DNS: https://developers.google.com/speed/public-dns
-        SocketAddr::from(([8, 8, 8, 8], 53)),
-        SocketAddr::from(([8, 8, 4, 4], 53)),
-    ];
+    // let addresses = [
+    //     // Cloudflare DNS: https://1.1.1.1/dns/
+    //     SocketAddr::from(([1, 1, 1, 1], 53)),
+    //     SocketAddr::from(([1, 0, 0, 1], 53)),
+    //     // Google DNS: https://developers.google.com/speed/public-dns
+    //     SocketAddr::from(([8, 8, 8, 8], 53)),
+    //     SocketAddr::from(([8, 8, 4, 4], 53)),
+    // ];
 
-    for address in addresses {
-        if let Ok(stream) = TcpStream::connect_timeout(&address, Duration::new(3, 0)) {
-            stream.shutdown(Shutdown::Both).unwrap();
+    // for address in addresses {
+    //     if let Ok(stream) = TcpStream::connect_timeout(&address, Duration::new(3, 0)) {
+    //         stream.shutdown(Shutdown::Both).unwrap();
 
-            return false;
-        }
-    }
+    //         return false;
+    //     }
+    // }
 
-    true
+    // true
 }
 
 pub fn is_test_env() -> bool {

417c00997075103ecbb600897387c1df3db922b7
nafees nazik <nafeesnazik21@gmail.com>
2022-07-04 22:31:30 +0530

    feat: add spinners

--- crates/cli/src/commands/teardown.rs
+++ crates/cli/src/commands/teardown.rs
@@ -1,9 +1,14 @@
+use indicatif::ProgressBar;
 use moon_workspace::Workspace;
 
 pub async fn teardown() -> Result<(), Box<dyn std::error::Error>> {
+    let pb = ProgressBar::new_spinner();
+    pb.set_message("Tearing down toolchain and uninstalling tools...");
+    pb.enable_steady_tick(20);
+
     let mut workspace = Workspace::load().await?;
 
     workspace.toolchain.teardown().await?;
-
+    pb.finish_with_message("Uninstalling complete");
     Ok(())
 }

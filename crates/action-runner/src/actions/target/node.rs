c686e0fe5f7bf8962a9a0858403af94c0e30b383
Miles Johnson <milesj@users.noreply.github.com>
2022-07-06 16:06:31 -0700

    internal: Run package binaries directly on Windows. (#182)
    * Add extraction.
    
    * Remove old windows code.
    
    * Test on windows.
    
    * Try without canonicalize.
    
    * Build fixes.
    
    * Add caching.

--- crates/action-runner/src/actions/target/node.rs
+++ crates/action-runner/src/actions/target/node.rs
@@ -1,6 +1,7 @@
 use crate::errors::ActionRunnerError;
 use moon_project::{Project, Task};
 use moon_toolchain::{get_path_env_var, Executable};
+use moon_utils::path::relative_from;
 use moon_utils::process::Command;
 use moon_utils::string_vec;
 use moon_workspace::Workspace;
@@ -24,7 +25,6 @@ fn create_node_options(task: &Task) -> Vec<String> {
 ///
 /// ~/.moon/tools/node/1.2.3/bin/node --inspect /path/to/node_modules/.bin/eslint
 ///     --cache --color --fix --ext .ts,.tsx,.js,.jsx
-#[cfg(not(windows))]
 #[track_caller]
 pub fn create_node_target_command(
     workspace: &Workspace,
@@ -51,7 +51,8 @@ pub fn create_node_target_command(
             cmd = node.get_yarn().unwrap().get_bin_path();
         }
         bin => {
-            let bin_path = node.find_package_bin(bin, &project.root)?;
+            let bin_path =
+                relative_from(node.find_package_bin(bin, &project.root)?, &project.root).unwrap();
 
             args.extend(create_node_options(task));
             args.push(path::path_to_string(&bin_path)?);
@@ -65,47 +66,6 @@ pub fn create_node_target_command(
         "PATH",
         get_path_env_var(node.get_bin_path().parent().unwrap()),
     );
-
-    Ok(command)
-}
-
-/// Windows works quite differently than other systems, so we cannot do the above.
-/// On Windows, the package binary is a ".cmd" file, which means it needs to run
-/// through "cmd.exe" and not "node.exe". Because of this, the order of operations
-/// is switched, and "node.exe" is detected through the `PATH` env var.
-#[cfg(windows)]
-#[track_caller]
-pub fn create_node_target_command(
-    workspace: &Workspace,
-    project: &Project,
-    task: &Task,
-) -> Result<Command, ActionRunnerError> {
-    use moon_lang_node::node;
-
-    let node = workspace.toolchain.get_node();
-
-    let cmd = match task.command.as_str() {
-        "node" => node.get_bin_path().clone(),
-        "npm" => node.get_npm().get_bin_path().clone(),
-        "pnpm" => node.get_pnpm().unwrap().get_bin_path().clone(),
-        "yarn" => node.get_yarn().unwrap().get_bin_path().clone(),
-        bin => node.find_package_bin(bin, &project.root)?,
-    };
-
-    // Create the command
-    let mut command = Command::new(cmd);
-
-    command
-        .args(&task.args)
-        .envs(&task.env)
-        .env(
-            "PATH",
-            get_path_env_var(node.get_bin_path().parent().unwrap()),
-        )
-        .env(
-            "NODE_OPTIONS",
-            node::extend_node_options_env_var(&create_node_options(task).join(" ")),
-        );
 
     Ok(command)
 }

b22261b189aeeb96a46d9bbf2255e607a133c391
Miles Johnson <mileswjohnson@gmail.com>
2022-07-06 18:04:29 -0700

    Add settings.

--- crates/config/src/workspace/typescript.rs
+++ crates/config/src/workspace/typescript.rs
@@ -6,9 +6,13 @@ use validator::Validate;
 #[schemars(default)]
 #[serde(rename_all = "camelCase")]
 pub struct TypeScriptConfig {
+    pub create_missing_config: bool,
+
     pub project_config_file_name: String,
 
     pub root_config_file_name: String,
+
+    pub root_options_config_file_name: String,
 
     pub sync_project_references: bool,
 }
@@ -16,8 +20,10 @@ pub struct TypeScriptConfig {
 impl Default for TypeScriptConfig {
     fn default() -> Self {
         TypeScriptConfig {
+            create_missing_config: true,
             project_config_file_name: String::from("tsconfig.json"),
             root_config_file_name: String::from("tsconfig.json"),
+            root_options_config_file_name: String::from("tsconfig.options.json"),
             sync_project_references: true,
         }
     }

672481ffee554a3d6b574298847b5c2b5f09200f
Miles Johnson <mileswjohnson@gmail.com>
2022-07-07 22:53:10 -0700

    Add tests.

--- crates/cli/src/commands/dep_graph.rs
+++ crates/cli/src/commands/dep_graph.rs
@@ -7,11 +7,19 @@ pub async fn dep_graph(target_id: &Option<String>) -> Result<(), Box<dyn std::er
     let projects = workspace.projects;
     let mut graph = DepGraph::default();
 
+    // Preload all projects
+    for id in projects.ids() {
+        projects.load(&id)?;
+    }
+
+    // Focus a target and its dependencies/dependents
     if let Some(id) = target_id {
         let target = Target::parse(id)?;
 
         graph.run_target(&target, &projects, None)?;
         graph.run_target_dependents(&target, &projects)?;
+
+    // Show all targets and actions
     } else {
         for project_id in projects.ids() {
             for task_id in projects.load(&project_id)?.tasks.keys() {

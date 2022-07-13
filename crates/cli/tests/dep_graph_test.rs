3f53092dea4b2fe72d151168e231316a2a2a2789
Miles Johnson <mileswjohnson@gmail.com>
2022-07-13 16:07:42 -0700

    Add tests.

--- crates/cli/tests/dep_graph_test.rs
+++ crates/cli/tests/dep_graph_test.rs
@@ -7,7 +7,7 @@ fn all_by_default() {
     let dot = get_assert_output(&assert);
 
     // Snapshot is not deterministic
-    assert_eq!(dot.split('\n').count(), 229);
+    assert_eq!(dot.split('\n').count(), 238);
 }
 
 #[test]

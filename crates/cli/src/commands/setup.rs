c1b211865f9e5eaed8d4d4c18d6999d3024ee6d5
Miles Johnson <mileswjohnson@gmail.com>
2022-07-23 00:57:48 -0700

    Revert "Install deps during setup."
    This reverts commit 60b7a75b753ed3940e0739ff907c56009c351606.

--- crates/cli/src/commands/setup.rs
+++ crates/cli/src/commands/setup.rs
@@ -1,21 +1,17 @@
 use indicatif::{ProgressBar, ProgressStyle};
 use moon_terminal::create_theme;
 use moon_workspace::Workspace;
-use moon_action_runner::{ActionRunner, DepGraph};
 
 pub async fn setup() -> Result<(), Box<dyn std::error::Error>> {
     let theme = create_theme();
-    let mut workspace = Workspace::load().await?;
 
     let pb = ProgressBar::new_spinner();
     pb.set_message("Downloading and installing tools...");
     pb.enable_steady_tick(20);
 
+    let mut workspace = Workspace::load().await?;
+
     workspace.toolchain.setup(true).await?;
-
-    pb.set_message("Installing dependencies...");
-
-    ActionRunner::new(workspace).run(DepGraph::for_setup(), None).await?;
 
     pb.set_style(ProgressStyle::default_spinner().template("{prefix} {msg}"));
     pb.set_prefix(theme.success_prefix.to_string());

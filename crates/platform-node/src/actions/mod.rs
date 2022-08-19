ac19046577c06f7fdc2a923865b653d34b46b99b
Miles Johnson <mileswjohnson@gmail.com>
2022-08-18 18:51:11 -0700

    Move run target.

--- crates/platform-node/src/actions/mod.rs
+++ crates/platform-node/src/actions/mod.rs
@@ -1,5 +1,7 @@
 mod install_deps;
+mod run_target;
 mod sync_project;
 
 pub use install_deps::install_deps;
+pub use run_target::*;
 pub use sync_project::sync_project;
--- crates/platform-system/src/lib.rs
+++ crates/platform-system/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod actions;
 mod hasher;
 
 pub use hasher::SystemTargetHasher;

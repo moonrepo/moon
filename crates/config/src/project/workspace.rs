494ce463f6a468189592887b0cf4671e9f705d46
Miles Johnson <mileswjohnson@gmail.com>
2022-09-30 15:25:16 -0700

    Add node config.

--- crates/config/src/project/workspace.rs
+++ crates/config/src/project/workspace.rs
@@ -1,11 +1,24 @@
 // These configs are project-level settings that override those from the workspace!
 
 use crate::types::TaskID;
-use crate::validators::skip_if_default;
+use crate::validators::{skip_if_default, validate_semver_version};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use validator::Validate;
+use validator::{Validate, ValidationError};
+
+fn validate_node_version(value: &str) -> Result<(), ValidationError> {
+    validate_semver_version("workspace.node.version", value)
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Eq, JsonSchema, PartialEq, Serialize, Validate)]
+#[schemars(default)]
+#[serde(default)]
+pub struct ProjectWorkspaceNodeConfig {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[validate(custom = "validate_node_version")]
+    pub version: Option<String>,
+}
 
 #[derive(Clone, Debug, Default, Deserialize, Eq, JsonSchema, PartialEq, Serialize, Validate)]
 #[schemars(default)]
@@ -29,6 +42,10 @@ pub struct ProjectWorkspaceConfig {
     #[validate]
     pub inherited_tasks: ProjectWorkspaceInheritedTasksConfig,
 
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[validate]
+    pub node: Option<ProjectWorkspaceNodeConfig>,
+
     pub typescript: bool,
 }
 
@@ -36,6 +53,7 @@ impl Default for ProjectWorkspaceConfig {
     fn default() -> Self {
         ProjectWorkspaceConfig {
             inherited_tasks: ProjectWorkspaceInheritedTasksConfig::default(),
+            node: None,
             typescript: true,
         }
     }

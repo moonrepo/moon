78defb0c81a60e3d38a42473dd57776d5f764397
Miles Johnson <mileswjohnson@gmail.com>
2022-10-03 18:39:45 -0700

    Add template.

---
+++ website/src/components/Docs/HeadingApiLink.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { faCode } from '@fortawesome/pro-regular-svg-icons';
+import Icon from '../../ui/iconography/Icon';
+
+export interface HeadingApiLinkProps {
+	to: string;
+}
+
+export default function HeadingApiLink({ to }: HeadingApiLinkProps) {
+	return (
+		<a
+			href={to}
+			target="_blank"
+			className="float-right inline-block"
+			style={{ marginTop: '-3.5em' }}
+		>
+			<Icon icon={faCode} />
+		</a>
+	);
+}

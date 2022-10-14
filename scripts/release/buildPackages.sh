e6e25ac76ab4d30f710f8a6dc38ad3e32a3a1d2f
Miles Johnson <mileswjohnson@gmail.com>
2022-10-14 14:20:44 -0700

    Try pnpm...

--- scripts/release/buildPackages.sh
+++ scripts/release/buildPackages.sh
@@ -4,10 +4,10 @@
 cd scripts
 
 # Build all packages with moon itself, so that the order is resolved correctly
-npm install @moonrepo/cli@latest
-./node_modules/@moonrepo/cli/moon run :build
-
-# npx --package @moonrepo/cli@latest -- moon run :build
+# npm install @moonrepo/cli@latest
+# ./node_modules/@moonrepo/cli/moon run :build
+npm install -g pnpm
+pnpm --package @moonrepo/cli@latest dlx moon run :build
 
 # # When not in CI, use npx
 # if [[ -z "${CI}" ]]; then

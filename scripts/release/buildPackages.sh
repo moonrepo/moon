bc6b6321ee71248f905cc1a47eb73496a9c4d392
Miles Johnson <mileswjohnson@gmail.com>
2022-10-14 14:27:33 -0700

    Add implicit deps.

--- scripts/release/buildPackages.sh
+++ scripts/release/buildPackages.sh
@@ -4,17 +4,7 @@
 cd scripts
 
 # Build all packages with moon itself, so that the order is resolved correctly
-# npm install @moonrepo/cli@latest
-# ./node_modules/@moonrepo/cli/moon run :build
 npm install -g pnpm
 pnpm --package @moonrepo/cli@latest dlx moon run :build
 
-# # When not in CI, use npx
-# if [[ -z "${CI}" ]]; then
-# 	npx --package @moonrepo/cli@latest -- moon run :build
-
-# # npx doesn't work in CI, so install directly
-# else
-# 	yarn add --dev @moonrepo/cli@latest
-# 	./node_modules/@moonrepo/cli/moon run :build
-# fi
+# Note: yarn/npm/npx did not work here, but pnpm does!

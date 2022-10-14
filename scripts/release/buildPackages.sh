66f062e118e3d4b4f545819b63fff6b599219846
Miles Johnson <mileswjohnson@gmail.com>
2022-10-14 12:25:08 -0700

    Update build.

--- scripts/release/buildPackages.sh
+++ scripts/release/buildPackages.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
 # Build all packages with moon itself, so that the order is resolved correctly
-npx --package @moonrepo/cli@latest -- moon run :build
+
+
+# When not in CI, use npx
+if [[ -z "${CI}" ]]; then
+	npx --package @moonrepo/cli@latest -- moon run :build
+
+# npx doesn't work in CI, so install directly
+else
+	yarn add --dev @moonrepo/cli@latest
+	./node_modules/@moonrepo/cli/moon run :build
+fi

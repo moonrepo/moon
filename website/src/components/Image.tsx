504d6b1f35c0f99fb6ee1a6d34c3fac759f304b2
Miles Johnson <mileswjohnson@gmail.com>
2022-10-17 09:29:55 -0700

    Start on doc page.

--- website/src/components/Image.tsx
+++ website/src/components/Image.tsx
@@ -5,11 +5,20 @@ export interface ImageProps {
 	width?: string;
 	alt?: string;
 	title?: string;
+	align?: 'center' | 'left' | 'right';
+	padding?: string;
 }
 
-export default function Image({ src, width = '90%', alt = '', title }: ImageProps) {
+export default function Image({
+	src,
+	width = '90%',
+	alt = '',
+	title,
+	align = 'center',
+	padding = '1rem',
+}: ImageProps) {
 	return (
-		<div style={{ paddingBottom: '1rem', paddingTop: '1rem', textAlign: 'center' }}>
+		<div style={{ paddingBottom: padding, paddingTop: padding, textAlign: align }}>
 			<img src={src.default} width={width} alt={alt} title={title} />
 		</div>
 	);

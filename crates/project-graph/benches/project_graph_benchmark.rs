985184fa42b1043648c58118332fcd29946ea113
Miles Johnson <milesj@users.noreply.github.com>
2022-10-21 22:27:11 -0700

    build: Add benchmarking infrastructure. (#385)
    * Add graph benchmark.
    
    * Add emitter benchmark.
    
    * Rename file.
    
    * Add runner benchmark.
    
    * Add workflow.
    
    * Remove if condition.
    
    * Fixes.
    
    * Turn off workflow.

---
+++ crates/project-graph/benches/project_graph_benchmark.rs
@@ -0,0 +1,62 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use moon_cache::CacheEngine;
+use moon_config::{GlobalProjectConfig, WorkspaceConfig, WorkspaceProjects};
+use moon_project_graph::ProjectGraph;
+use moon_utils::test::get_fixtures_dir;
+use std::collections::HashMap;
+
+pub fn load_benchmark(c: &mut Criterion) {
+    let workspace_root = get_fixtures_dir("cases");
+    let workspace_config = WorkspaceConfig {
+        projects: WorkspaceProjects::Map(HashMap::from([("base".to_owned(), "base".to_owned())])),
+        ..WorkspaceConfig::default()
+    };
+
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+
+    runtime.block_on(async {
+        let cache = CacheEngine::create(&workspace_root).await.unwrap();
+
+        let graph = ProjectGraph::create(
+            &workspace_root,
+            &workspace_config,
+            GlobalProjectConfig::default(),
+            &cache,
+        )
+        .await
+        .unwrap();
+
+        c.bench_function("load", |b| {
+            b.iter(|| {
+                // This clones a new project struct every time
+                graph.load("base").unwrap();
+            })
+        });
+    });
+}
+
+pub fn load_all_benchmark(c: &mut Criterion) {
+    let workspace_root = get_fixtures_dir("cases");
+    let workspace_config = WorkspaceConfig::default();
+
+    c.bench_function("load_all", |b| {
+        b.to_async(tokio::runtime::Runtime::new().unwrap())
+            .iter(|| async {
+                let cache = CacheEngine::create(&workspace_root).await.unwrap();
+                let graph = ProjectGraph::create(
+                    &workspace_root,
+                    &workspace_config,
+                    GlobalProjectConfig::default(),
+                    &cache,
+                )
+                .await
+                .unwrap();
+
+                // This does NOT clone but inserts all projects into the graph
+                graph.load_all().unwrap();
+            })
+    });
+}
+
+criterion_group!(project_graph, load_benchmark, load_all_benchmark);
+criterion_main!(project_graph);

f6dbccce04866cf1506d2c63e18619374aedc3dc
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-10-23 03:59:25 +0000

    tests: dirty repo should be checked before migrate

--- crates/cli/tests/migrate_test.rs
+++ crates/cli/tests/migrate_test.rs
@@ -1,9 +1,25 @@
 use insta::assert_snapshot;
-use moon_utils::test::{create_moon_command, create_sandbox};
+use moon_utils::test::{create_moon_command, create_sandbox, create_sandbox_with_git};
+use predicates::str::contains;
 use std::fs;
 
 mod from_package_json {
     use super::*;
+
+    #[test]
+    fn dirty_repository_raises_an_error() {
+        let fixture = create_sandbox_with_git("migrate");
+        // create a new file at fixture path to simulate a dirty repository
+        fs::write(fixture.path().join("new_file"), "new_file").unwrap();
+        let assert = create_moon_command(fixture.path())
+            .args(["migrate", "from-package-json", "common"])
+            .assert();
+        assert
+            .failure()
+            .code(1)
+            .stdout("")
+            .stderr(contains("Commit or stash"));
+    }
 
     #[test]
     fn converts_scripts() {

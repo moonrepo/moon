bc2f674e79849e93bf209f56e6813e0cba8e5052
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-10-23 02:54:25 +0000

    fix: add missing return before error message

--- crates/cli/src/commands/migrate/mod.rs
+++ crates/cli/src/commands/migrate/mod.rs
@@ -5,7 +5,7 @@ use moon_workspace::Workspace;
 
 pub async fn check_dirty_repo(workspace: &Workspace) -> Result<(), Box<dyn std::error::Error>> {
     if !workspace.vcs.get_touched_files().await?.all.is_empty() {
-        Err("Commit or stash your changes before running this command, or use the `--skipTouchedFilesCheck` flag to disable this check.".to_string())?;
+        return Err("Commit or stash your changes before running this command, or use the `--skipTouchedFilesCheck` flag to disable this check.".to_string())?;
     }
     Ok(())
 }

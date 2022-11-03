9b2bc2d5a98bc802acf9eea33eb7a60d14a7540b
Miles Johnson <milesj@users.noreply.github.com>
2022-11-02 18:14:15 -0700

    fix: Fix passthrough args being passed to dependent targets. (#415)
    * Add new logic.
    
    * Add tests.
    
    * Update version.

--- crates/core/action/src/context.rs
+++ crates/core/action/src/context.rs
@@ -9,9 +9,11 @@ pub enum ProfileType {
     Heap,
 }
 
-#[derive(Default, Deserialize, Serialize)]
+#[derive(Debug, Default, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ActionContext {
+    pub initial_targets: FxHashSet<String>,
+
     pub passthrough_args: Vec<String>,
 
     pub primary_targets: FxHashSet<String>,
@@ -19,4 +21,26 @@ pub struct ActionContext {
     pub profile: Option<ProfileType>,
 
     pub touched_files: FxHashSet<PathBuf>,
+}
+
+impl ActionContext {
+    pub fn should_inherit_args(&self, target: &str) -> bool {
+        if self.passthrough_args.is_empty() {
+            return false;
+        }
+
+        // project:task == project:task
+        if self.primary_targets.contains(target) {
+            return true;
+        }
+
+        // :task == project:task
+        for initial_target in &self.initial_targets {
+            if target.ends_with(initial_target) {
+                return true;
+            }
+        }
+
+        false
+    }
 }

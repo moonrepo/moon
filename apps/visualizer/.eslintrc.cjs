0fd2ad02d15a9aacd992011234a044f2f6d507ba
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-11-06 17:24:13 +0000

    ci(visualizer): disable annoying eslint rule

--- apps/visualizer/.eslintrc.cjs
+++ apps/visualizer/.eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
 		'@typescript-eslint/no-unsafe-argument': 'off',
 		'@typescript-eslint/no-unsafe-assignment': 'off',
 		'@typescript-eslint/triple-slash-reference': 'off',
+		'@typescript-eslint/no-unsafe-member-access': 'off',
 	},
 	settings: {
 		react: { version: 'detect' },

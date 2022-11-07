b6f70abc87ca5e2d749ae8a63f459d5bb825f049
Diptesh Choudhuri <ignisda2001@gmail.com>
2022-11-07 09:56:37 +0000

    ci(visualizer): manually recreate gitignore on building

---
+++ apps/visualizer/plugins/createGitignore.ts
@@ -0,0 +1,19 @@
+import { writeFileSync } from 'fs'
+import type { PluginOption } from 'vite'
+import { normalizePath } from 'vite'
+
+/* The `dist` directory is deleted every time the bundle is rebuilt. The `.gitignore` file
+inside it needs to be generated again for the folder to remain committed to git. Hence we
+use vite's hook mechanism to manually re-create one. */
+export function createGitignore(): PluginOption  {
+    return {
+        apply: 'build',
+        closeBundle: ()  =>{
+            writeFileSync(normalizePath('dist/.gitignore'), `
+*
+!.gitignore
+`.trim())
+        },
+        name: 'create-gitignore',
+    }
+}
--- apps/visualizer/vite.config.ts
+++ apps/visualizer/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createGitignore } from './plugins/createGitignore';
 
 // https://vitejs.dev/config/
 /* eslint-disable import/no-default-export */
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), createGitignore()],
 });

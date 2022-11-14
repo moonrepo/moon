5eeccc294e708474bc3355dee8aab0b47492d478
Miles Johnson <milesj@users.noreply.github.com>
2022-11-13 20:45:20 -0800

    fix: Fix project override config defaults. (#438)
    * Update impl.
    
    * Update changelog.

--- crates/core/config/src/workspace/node.rs
+++ crates/core/config/src/workspace/node.rs
@@ -202,3 +202,16 @@ impl Default for NodeConfig {
         }
     }
 }
+
+impl NodeConfig {
+    pub fn with_project_override(&self, version: &str) -> Self {
+        let mut config = self.clone();
+        config.version = version.to_owned();
+
+        // These settings should not be ran in a project, only the root
+        config.add_engines_constraint = false;
+        config.sync_version_manager_config = None;
+
+        config
+    }
+}

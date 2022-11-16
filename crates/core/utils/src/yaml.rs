16c6fbc20e771c332c796f7dadaf587bab7e6798
Miles Johnson <milesj@users.noreply.github.com>
2022-11-15 18:22:44 -0800

    internal: Drop yaml-rust in favor of serde_yaml. (#448)
    * Update api.
    
    * Update more impl.

--- crates/core/utils/src/yaml.rs
+++ crates/core/utils/src/yaml.rs
@@ -4,10 +4,7 @@ use serde::Serialize;
 use std::fs;
 use std::path::Path;
 
-pub use yaml_rust::{
-    yaml::{Array, Hash},
-    Yaml, YamlEmitter, YamlLoader,
-};
+pub use serde_yaml::{Mapping, Value as YamlValue};
 
 #[inline]
 pub fn read<P, D>(path: P) -> Result<D, MoonError>
@@ -19,21 +16,7 @@ where
     let contents =
         fs::read_to_string(path).map_err(|e| map_io_to_fs_error(e, path.to_path_buf()))?;
 
-    let yaml: D =
-        serde_yaml::from_str(&contents).map_err(|e| map_yaml_to_error(e, path.to_path_buf()))?;
-
-    Ok(yaml)
-}
-
-#[inline]
-pub fn read_raw<T: AsRef<Path>>(path: T) -> Result<Yaml, MoonError> {
-    let path = path.as_ref();
-    let data = fs::read_to_string(path).map_err(|e| map_io_to_fs_error(e, path.to_path_buf()))?;
-    let docs = YamlLoader::load_from_str(&data).map_err(|e| MoonError::Generic(e.to_string()))?;
-
-    docs.into_iter()
-        .next()
-        .ok_or_else(|| MoonError::Generic("Invalid YAML document.".into()))
+    serde_yaml::from_str(&contents).map_err(|e| map_yaml_to_error(e, path.to_path_buf()))
 }
 
 #[inline]
@@ -53,16 +36,6 @@ where
 
 // This function is used for consumer facing files, like configs.
 #[inline]
-pub fn write_raw<P: AsRef<Path>>(path: P, yaml: Yaml) -> Result<(), MoonError> {
-    let path = path.as_ref();
-    let mut data = String::new();
-    let mut emitter = YamlEmitter::new(&mut data);
-
-    emitter
-        .dump(&yaml)
-        .map_err(|e| MoonError::Generic(e.to_string()))?;
-
-    fs::write(path, data).map_err(|e| map_io_to_fs_error(e, path.to_path_buf()))?;
-
-    Ok(())
+pub fn write_with_config<P: AsRef<Path>>(path: P, yaml: YamlValue) -> Result<(), MoonError> {
+    write(path, &yaml)
 }

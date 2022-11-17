b215616c3db6565b199c0b8e1b3c69a8fb723887
Miles Johnson <milesj@users.noreply.github.com>
2022-11-16 18:43:13 -0800

    new: Support `.editorconfig` for YAML files. (#451)
    * Impl.
    
    * Add tests.
    
    * Update changelog.
    
    * Add faq.

--- crates/core/utils/src/yaml.rs
+++ crates/core/utils/src/yaml.rs
@@ -1,10 +1,17 @@
-use moon_error::{map_io_to_fs_error, map_yaml_to_error, MoonError};
+use lazy_static::lazy_static;
+use moon_error::MoonError;
+use moon_error::{map_io_to_fs_error, map_yaml_to_error};
+use regex::Regex;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 use std::fs;
 use std::path::Path;
 
 pub use serde_yaml::{Mapping, Value as YamlValue};
+
+lazy_static! {
+    static ref LINE_WS_START: Regex = Regex::new(r"^(\s+)").unwrap();
+}
 
 #[inline]
 pub fn read<P, D>(path: P) -> Result<D, MoonError>
@@ -37,5 +44,40 @@ where
 // This function is used for consumer facing files, like configs.
 #[inline]
 pub fn write_with_config<P: AsRef<Path>>(path: P, yaml: YamlValue) -> Result<(), MoonError> {
-    write(path, &yaml)
+    let path = path.as_ref();
+    let editor_config = crate::fs::get_editor_config_props(path);
+
+    let mut data = serde_yaml::to_string(&yaml)
+        .map_err(|e| map_yaml_to_error(e, path.to_path_buf()))?
+        .trim()
+        .to_string();
+
+    // serde_yaml does not support customizing the indentation character. So to work around
+    // this, we do it manually on the YAML string, but only if the indent is different than
+    // a double space (the default). Can be customized with `.editorconfig`.
+    if editor_config.indent != "  " {
+        data = data
+            .split("\n")
+            .map(|line| {
+                if !line.starts_with("  ") {
+                    return line.to_string();
+                }
+
+                LINE_WS_START
+                    .replace_all(&line, |caps: &regex::Captures| {
+                        editor_config
+                            .indent
+                            .repeat(caps.get(1).unwrap().as_str().len() / 2)
+                    })
+                    .to_string()
+            })
+            .collect::<Vec<_>>()
+            .join("\n");
+    }
+
+    data += &editor_config.eof;
+
+    fs::write(path, data).map_err(|e| map_io_to_fs_error(e, path.to_path_buf()))?;
+
+    Ok(())
 }

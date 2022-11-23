2bb874d1a424a99f7000cd183c79fdb4583e0636
Miles Johnson <mileswjohnson@gmail.com>
2022-11-23 10:45:47 -0800

    Fix lints.

--- crates/core/pipeline-provider/src/aws_codebuild.rs
+++ crates/core/pipeline-provider/src/aws_codebuild.rs
@@ -7,26 +7,16 @@ pub fn create_environment() -> PipelineEnvironment {
         base_branch: opt_var("CODEBUILD_WEBHOOK_BASE_REF"),
         branch: opt_var("CODEBUILD_WEBHOOK_HEAD_REF")
             .or_else(|| match &trigger {
-                Some(value) => {
-                    if value.starts_with("branch/") {
-                        Some(value[7..].to_owned())
-                    } else {
-                        None
-                    }
-                }
+                Some(value) => value
+                    .strip_prefix("branch/")
+                    .map(|branch| branch.to_owned()),
                 None => None,
             })
             .unwrap_or_default(),
         id: var("CODEBUILD_BUILD_ID"),
         provider: PipelineProvider::AwsCodebuild,
         request_id: match &trigger {
-            Some(value) => {
-                if value.starts_with("pr/") {
-                    Some(value[3..].to_owned())
-                } else {
-                    None
-                }
-            }
+            Some(value) => value.strip_prefix("pr/").map(|pr| pr.to_owned()),
             None => None,
         },
         request_url: None,

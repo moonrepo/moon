4b64ce18489493420e6b999e1d3c1d0d5dd6c044
Miles Johnson <mileswjohnson@gmail.com>
2022-11-28 13:11:19 -0800

    Migrate migrate.

--- crates/cli/tests/migrate_test.rs
+++ crates/cli/tests/migrate_test.rs
@@ -1,5 +1,5 @@
 use insta::assert_snapshot;
-use moon_utils::test::{create_moon_command, create_sandbox, create_sandbox_with_git};
+use moon_test_utils::create_sandbox;
 use predicates::str::contains;
 use std::fs;
 
@@ -8,12 +8,16 @@ mod from_package_json {
 
     #[test]
     fn dirty_repository_raises_an_error() {
-        let fixture = create_sandbox_with_git("migrate");
-        // create a new file at fixture path to simulate a dirty repository
-        fs::write(fixture.path().join("new_file"), "new_file").unwrap();
-        let assert = create_moon_command(fixture.path())
-            .args(["migrate", "from-package-json", "common"])
-            .assert();
+        let mut sandbox = create_sandbox("migrate");
+        sandbox.enable_git();
+
+        // create a new file at sandbox path to simulate a dirty repository
+        fs::write(sandbox.path().join("new_file"), "new_file").unwrap();
+
+        let assert = sandbox.run_moon(|cmd| {
+            cmd.args(["migrate", "from-package-json", "common"]);
+        });
+
         assert
             .failure()
             .code(1)
@@ -23,24 +27,24 @@ mod from_package_json {
 
     #[test]
     fn converts_scripts() {
-        let fixture = create_sandbox("migrate");
+        let mut sandbox = create_sandbox("migrate");
 
-        let assert = create_moon_command(fixture.path())
-            .args([
+        let assert = sandbox.run_moon(|cmd| {
+            cmd.args([
                 "migrate",
                 "--skipTouchedFilesCheck",
                 "from-package-json",
                 "common",
-            ])
-            .assert();
+            ]);
+        });
 
         assert_snapshot!(fs::read_to_string(
-            fixture.path().join("package-json/common/package.json")
+            sandbox.path().join("package-json/common/package.json")
         )
         .unwrap());
 
         assert_snapshot!(
-            fs::read_to_string(fixture.path().join("package-json/common/moon.yml")).unwrap()
+            fs::read_to_string(sandbox.path().join("package-json/common/moon.yml")).unwrap()
         );
 
         assert.success();
@@ -48,23 +52,23 @@ mod from_package_json {
 
     #[test]
     fn links_depends_on() {
-        let fixture = create_sandbox("migrate");
+        let mut sandbox = create_sandbox("migrate");
 
-        let assert = create_moon_command(fixture.path())
-            .args([
+        let assert = sandbox.run_moon(|cmd| {
+            cmd.args([
                 "migrate",
                 "--skipTouchedFilesCheck",
                 "from-package-json",
                 "deps",
-            ])
-            .assert();
+            ]);
+        });
 
         assert_snapshot!(
-            fs::read_to_string(fixture.path().join("package-json/deps/package.json")).unwrap()
+            fs::read_to_string(sandbox.path().join("package-json/deps/package.json")).unwrap()
         );
 
         assert_snapshot!(
-            fs::read_to_string(fixture.path().join("package-json/deps/moon.yml")).unwrap()
+            fs::read_to_string(sandbox.path().join("package-json/deps/moon.yml")).unwrap()
         );
 
         assert.success();

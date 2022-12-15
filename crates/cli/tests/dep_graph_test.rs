31d936d93602a922c96550acaebc302a23feb6e7
Miles Johnson <mileswjohnson@gmail.com>
2022-12-15 15:43:37 -0800

    Fix test.

--- crates/cli/tests/dep_graph_test.rs
+++ crates/cli/tests/dep_graph_test.rs
@@ -21,7 +21,7 @@ fn all_by_default() {
     let dot = assert.output();
 
     // Snapshot is not deterministic
-    assert_eq!(dot.split('\n').count(), 445);
+    assert_eq!(dot.split('\n').count(), 446);
 }
 
 #[test]

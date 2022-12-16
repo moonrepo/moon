e2f6e2516b83de809a7b8d1ca3e7012ad3957f65
Miles Johnson <mileswjohnson@gmail.com>
2022-12-16 11:40:46 -0800

    Add ruby crate.

---
+++ crates/ruby/lang/src/lib.rs
@@ -0,0 +1,31 @@
+use moon_lang::{DependencyManager, Language, VersionManager};
+
+pub const RUBY: Language = Language {
+    binary: "ruby",
+    default_version: "3.1.3",
+    file_exts: &["rb"],
+    vendor_bins_dir: "",
+    vendor_dir: "vendor",
+};
+
+// Package managers
+
+pub const BUNDLER: DependencyManager = DependencyManager {
+    binary: "bundle",
+    config_files: &[".bundle/config"],
+    default_version: "2.3.0",
+    lockfile: "Gemfile.lock",
+    manifest: "Gemfile",
+};
+
+// Version managers
+
+pub const RVM: VersionManager = VersionManager {
+    binary: "rvm",
+    version_file: ".ruby-version",
+};
+
+pub const RBENV: VersionManager = VersionManager {
+    binary: "rbenv",
+    version_file: ".ruby-version",
+};

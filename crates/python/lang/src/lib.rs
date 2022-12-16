a3ef3d8aa3dfc8c46716d6148ca90095c2ef4234
Miles Johnson <mileswjohnson@gmail.com>
2022-12-16 11:23:17 -0800

    Add python crate.

---
+++ crates/python/lang/src/lib.rs
@@ -0,0 +1,34 @@
+use moon_lang::{DependencyManager, Language, VersionManager};
+
+pub const PYTHON: Language = Language {
+    binary: "python",
+    default_version: "3.11.1",
+    file_exts: &["py", "pyc", "pyo", "pyd"],
+    vendor_bins_dir: "",
+    vendor_dir: "",
+};
+
+// Package managers
+
+pub const PIP: DependencyManager = DependencyManager {
+    binary: "pip",
+    config_files: &["constraints.txt"],
+    default_version: "22.3.1",
+    lockfile: "",
+    manifest: "requirements.txt",
+};
+
+pub const PIPENV: DependencyManager = DependencyManager {
+    binary: "pipenv",
+    config_files: &[],
+    default_version: "2022.11.30",
+    lockfile: "Pipfile.lock",
+    manifest: "Pipfile",
+};
+
+// Version managers
+
+pub const PYENV: VersionManager = VersionManager {
+    binary: "pyenv",
+    version_file: ".python-version",
+};

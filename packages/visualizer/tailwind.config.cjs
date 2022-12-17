e073d655d38cf7ef5d7f56172cb22e8cb05434d0
Miles Johnson <mileswjohnson@gmail.com>
2022-12-17 14:30:36 -0800

    Update deps.

--- packages/visualizer/tailwind.config.cjs
+++ packages/visualizer/tailwind.config.cjs
@@ -2,6 +2,6 @@ const workspacePreset = require('../../tailwind.config');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	presets: [workspacePreset],
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	presets: [workspacePreset],
 };

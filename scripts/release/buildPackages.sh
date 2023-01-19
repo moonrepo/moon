2cb0b68845355abad0bb18ef0afcd51eacc8cf11
Miles Johnson <mileswjohnson@gmail.com>
2023-01-18 21:03:23 -0800

    Try a diff version.

--- scripts/release/buildPackages.sh
+++ scripts/release/buildPackages.sh
@@ -5,7 +5,7 @@ cd scripts
 
 # Build all packages with moon itself, so that the order is resolved correctly
 npm install -g pnpm
-pnpm --package @moonrepo/cli@0.22.0 dlx moon run types:build visualizer:build
-pnpm --package @moonrepo/cli@0.22.0 dlx moon run report:build runtime:build visualizer:build
+pnpm --package @moonrepo/cli@0.21.0 dlx moon run types:build visualizer:build
+pnpm --package @moonrepo/cli@0.21.0 dlx moon run report:build runtime:build visualizer:build
 
 # Note: yarn/npm/npx did not work here, but pnpm does!

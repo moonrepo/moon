af446cb86dca14d3cae4b1da78c1fe6173e79c1c
Miles Johnson <mileswjohnson@gmail.com>
2023-02-03 21:27:09 -0800

    Start on new homepage.

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,187 +1,9 @@
-/* eslint-disable sort-keys */
-
 import React from 'react';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import {
-	faAperture,
-	faArrowUpRightDots,
-	faBellOn,
-	faBoxAlt,
-	faChartTreeMap,
-	faCircleBolt,
-	faCloudArrowUp,
-	faDiagramProject,
-	faDiagramSankey,
-	faFingerprint,
-	faGridDividers,
-	faLayerGroup,
-	faMerge,
-	faMessageCode,
-	faMicrochip,
-	faRectangleBarcode,
-	faShieldHalved,
-	faSitemap,
-	faSlidersUp,
-	faToolbox,
-	faUserSecret,
-} from '@fortawesome/pro-duotone-svg-icons';
-import AdditionalFeatures from '@site/src/components/Home/AdditionalFeatures';
-import Features, { Feature } from '@site/src/components/Home/Features';
 import Hero from '@site/src/components/Home/Hero';
 import UsedBy from '@site/src/components/Home/UsedBy';
 import Layout from '@theme/Layout';
-
-const managementFeatures: Feature[] = [
-	{
-		title: 'Smart hashing',
-		icon: faFingerprint,
-		description:
-			'Collects inputs from multiple sources to ensure builds are deterministic and reproducible.',
-	},
-	{
-		title: 'Remote caching',
-		icon: faCloudArrowUp,
-		description: 'Persists builds, hashes, and caches between teammates and CI/CD environments.',
-		status: 'new',
-	},
-	{
-		title: 'Integrated toolchain',
-		icon: faToolbox,
-		description:
-			'Automatically downloads and installs explicit versions of Node.js and other tools for consistency across the entire workspace or per project.',
-	},
-	{
-		title: 'Multi-platform',
-		icon: faMicrochip,
-		description: 'Runs on common development platforms: Linux, macOS, and Windows.',
-	},
-];
-
-const organizationFeatures: Feature[] = [
-	{
-		title: 'Project graph',
-		icon: faDiagramProject,
-		description: 'Generates a project graph for dependency and dependent relationships.',
-	},
-	{
-		title: 'Code generation',
-		icon: faLayerGroup,
-		description: 'Easily scaffold new applications, libraries, tooling, and more!',
-	},
-	{
-		title: 'Dependency workspaces',
-		icon: faGridDividers,
-		description:
-			'Works alongside package manager workspaces so that projects have distinct dependency trees.',
-	},
-	{
-		title: 'Ownership metadata',
-		icon: faRectangleBarcode,
-		description:
-			'Declare an owner, maintainers, support channels, and more, for LDAP or another integration.',
-	},
-];
-
-const orchestrationFeatures: Feature[] = [
-	{
-		title: 'Dependency graph',
-		icon: faSitemap,
-		description: 'Generates a dependency graph to increase performance and reduce workloads.',
-	},
-	{
-		title: 'Action pipeline',
-		icon: faMerge,
-		description:
-			'Executes actions in parallel and in order using a thread pool and our dependency graph.',
-	},
-	{
-		title: 'Action distribution',
-		icon: faDiagramSankey,
-		description: 'Distributes actions across multiple machines to increase throughput.',
-		status: 'coming-soon',
-	},
-	{
-		title: 'Incremental builds',
-		icon: faArrowUpRightDots,
-		description:
-			'With our smart hashing, only rebuild projects that have been touched since the last build.',
-	},
-];
-
-const notificationFeatures: Feature[] = [
-	{
-		title: 'Flakiness detection',
-		icon: faShieldHalved,
-		description: 'Reduce flaky builds with automatic retries and passthrough settings.',
-		status: 'experimental',
-	},
-	{
-		title: 'Webhook events',
-		icon: faMessageCode,
-		description:
-			'Receive a webhook for every event in the pipeline. Useful for metrics gathering and insights.',
-		status: 'experimental',
-	},
-	{
-		title: 'Terminal notifications',
-		icon: faBellOn,
-		description:
-			'Receives notifications in your chosen terminal when builds are successful... or are not.',
-		status: 'coming-soon',
-	},
-];
-
-const additionalFeatures: Feature[] = [
-	{
-		title: 'Configuration & convention',
-		icon: faSlidersUp,
-		description: 'Use moon the way you want, but with some guard rails.',
-	},
-	{
-		title: 'Scalability aware',
-		icon: faCircleBolt,
-		description: 'Engineered to scale and grow for codebases of any size.',
-	},
-	{
-		title: 'Integrated packages',
-		icon: faBoxAlt,
-		description: (
-			<>
-				Enhance your pipeline with our{' '}
-				<Link href="https://www.npmjs.com/org/moonrepo">@moonrepo</Link> npm packages.
-			</>
-		),
-		status: 'in-development',
-	},
-	{
-		title: 'Node.js inspection',
-		icon: faUserSecret,
-		description: 'Inspect and debug failing Node.js processes.',
-		status: 'coming-soon',
-	},
-	{
-		title: 'Build profiles',
-		icon: faAperture,
-		description: (
-			<>
-				Record <Link href="/docs/guides/profile">CPU and heap profiles</Link> that can be analyzed
-				in Chrome.
-			</>
-		),
-	},
-	{
-		title: 'Editor extensions',
-		icon: faChartTreeMap,
-		description: (
-			<>
-				Utilize moon extensions in your favorite editor, like{' '}
-				<Link href="/docs/editors/vscode">Visual Studio Code</Link>.
-			</>
-		),
-		status: 'new',
-	},
-];
 
 const description =
 	'Reduces build times and repository maintenance costs through high-quality developer tooling.';
@@ -191,42 +13,29 @@ export default function Home() {
 
 	return (
 		<Layout title={siteConfig.tagline} description={description}>
-			<Hero />
+			<div className="bg-gradient-to-b from-slate-900 to-slate-600 star-pattern">
+				<div className="max-w-7xl mx-auto py-10 px-4 pb-6 sm:py-12 sm:px-6 md:py-14 lg:py-16 lg:px-8 xl:py-20 flex flex-col justify-center items-center">
+					<h1 className="text-2xl tracking-tight font-extrabold text-purple-600">
+						<img
+							src="/img/logo-hero.svg"
+							alt={siteConfig.title}
+							title={siteConfig.title}
+							width={200}
+						/>
+					</h1>
 
-			<main>
-				<Features
-					header="Management"
-					description="Develop more, manage less"
-					features={managementFeatures}
-					tier={1}
-				/>
+					<h2 className="mt-1 mb-3 text-white font-normal text-center text-2xl sm:text-3xl md:text-5xl">
+						Next generation productivity tooling
+					</h2>
 
-				<Features
-					header="Organization"
-					description="Architect a repository to scale"
-					features={organizationFeatures}
-					tier={2}
-					reversed
-				/>
+					<p className="mm-0 text-white text-base text-center opacity-60 sm:text-lg md:text-xl md:max-w-xl">
+						moonrepo is a better way to manage codebases, organize projects, orchestrate tasks, and
+						scale your business.
+					</p>
+				</div>
+			</div>
 
-				<Features
-					header="Orchestration"
-					description="Offload heavy tasks"
-					features={orchestrationFeatures}
-					tier={3}
-				/>
-
-				<Features
-					header="Notification"
-					description="Monitor pipeline health"
-					features={notificationFeatures}
-					// columns={3}
-					tier={4}
-					reversed
-				/>
-
-				<AdditionalFeatures header="And many more features" features={additionalFeatures} />
-
+			<main className="bg-gradient-to-b from-slate-600 via-purple-600 to-white">
 				<UsedBy />
 			</main>
 		</Layout>

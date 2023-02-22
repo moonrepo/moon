6e0d445107e1a4cd592cf9fe60d42078d9f3909f
Miles Johnson <mileswjohnson@gmail.com>
2023-02-22 10:47:55 -0800

    Implement display manually.

--- crates/core/config/src/project/language_platform.rs
+++ crates/core/config/src/project/language_platform.rs
@@ -1,46 +1,49 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use std::fmt;
 use strum::{Display, EnumIter};
 
-#[derive(
-    Clone, Debug, Default, Deserialize, Display, EnumIter, Eq, JsonSchema, PartialEq, Serialize,
-)]
+#[derive(Clone, Debug, Default, Deserialize, EnumIter, Eq, JsonSchema, PartialEq, Serialize)]
 #[serde(rename_all = "lowercase")]
 pub enum ProjectLanguage {
-    #[strum(serialize = "bash")]
     Bash,
-
-    #[strum(serialize = "batch")]
     Batch,
-
-    #[strum(serialize = "go")]
     Go,
-
-    #[strum(serialize = "javascript")]
     JavaScript,
-
-    #[strum(serialize = "php")]
     Php,
-
-    #[strum(serialize = "python")]
     Python,
-
-    #[strum(serialize = "ruby")]
     Ruby,
-
-    #[strum(serialize = "rust")]
     Rust,
-
-    #[strum(serialize = "typescript")]
     TypeScript,
 
     // Not explicitly set or detected
     #[default]
-    #[strum(serialize = "unknown")]
     Unknown,
 
     // An unsupported language
     Other(String),
+}
+
+impl fmt::Display for ProjectLanguage {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                ProjectLanguage::Bash => "bash",
+                ProjectLanguage::Batch => "batch",
+                ProjectLanguage::Go => "go",
+                ProjectLanguage::JavaScript => "javascript",
+                ProjectLanguage::Php => "php",
+                ProjectLanguage::Python => "python",
+                ProjectLanguage::Ruby => "ruby",
+                ProjectLanguage::Rust => "rust",
+                ProjectLanguage::TypeScript => "typescript",
+                ProjectLanguage::Unknown => "unknown",
+                ProjectLanguage::Other(lang) => lang,
+            }
+        )
+    }
 }
 
 #[derive(
@@ -94,5 +97,22 @@ impl From<ProjectLanguage> for PlatformType {
             | ProjectLanguage::Rust
             | ProjectLanguage::Other(_) => PlatformType::System,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn converts_lang_to_string() {
+        assert_eq!(ProjectLanguage::Go.to_string(), "go");
+        assert_eq!(ProjectLanguage::JavaScript.to_string(), "javascript");
+        assert_eq!(ProjectLanguage::Ruby.to_string(), "ruby");
+        assert_eq!(ProjectLanguage::Unknown.to_string(), "unknown");
+        assert_eq!(
+            ProjectLanguage::Other("dotnet".into()).to_string(),
+            "dotnet"
+        );
     }
 }

70aff82d500cde8a50c5a98ff445adf28d28681e
Miles Johnson <mileswjohnson@gmail.com>
2023-03-21 11:35:27 -0700

    Clean up errors.

--- crates/core/enforcer/src/errors.rs
+++ crates/core/enforcer/src/errors.rs
@@ -3,9 +3,9 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum EnforcerError {
-    #[error("Invalid project relationship. Project {0} of type {1} cannot depend on project {2} of type {3}. Projects can only depend on libraries.")]
+    #[error("Invalid project relationship. Project <id>{0}</id> of type {1} cannot depend on project <id>{2}</id> of type {3}; can only depend on libraries.")]
     InvalidTypeRelationship(String, ProjectType, String, ProjectType),
 
-    #[error("Invalid tag relationship. Project {0} with tag {1} cannot depend on project {2}. The tag {1} requires a dependency to have one of the following tags: {3}.")]
+    #[error("Invalid tag relationship. Project <id>{0}</id> with tag {1} cannot depend on project <id>{2}</id>. The tag {1} requires a dependency to have one of the following tags: {3}.")]
     InvalidTagRelationship(String, String, String, String),
 }

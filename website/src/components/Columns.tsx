9110ab059b46439875fbf30126fe17e34a4fd71e
Miles Johnson <mileswjohnson@gmail.com>
2023-03-25 15:59:10 -0700

    Update blog.

---
+++ website/src/components/Columns.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import clsx from 'clsx';
+
+export interface ColumnsProps {
+	children: React.ReactNode;
+	count: 2 | 3;
+}
+
+export default function Columns({ children, count }: ColumnsProps) {
+	return (
+		<section
+			className={clsx('sm:grid gap-3', count === 2 && 'grid-cols-2', count === 3 && 'grid-cols-3')}
+		>
+			{children}
+		</section>
+	);
+}

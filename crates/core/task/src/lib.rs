5b37d32d73789a724b43015dcecfeee7a8afe469
Miles Johnson <mileswjohnson@gmail.com>
2023-05-08 14:06:21 -0700

    Delete old group.

--- crates/core/task/src/lib.rs
+++ crates/core/task/src/lib.rs
@@ -1,11 +1,9 @@
 mod errors;
-mod file_group;
 mod task;
 mod task_options;
 mod types;
 
 pub use errors::*;
-pub use file_group::*;
 pub use moon_config::{PlatformType, TargetID, TaskConfig, TaskID, TaskOptionsConfig};
 pub use task::*;
 pub use task_options::*;

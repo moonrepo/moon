aa17e257ea8a6651d936ae5a05fe570bf8b6835d
Miles Johnson <mileswjohnson@gmail.com>
2023-05-24 11:37:07 -0700

    Update task.

--- nextgen/config/src/portable_path.rs
+++ nextgen/config/src/portable_path.rs
@@ -225,3 +225,13 @@ impl<'de> Deserialize<'de> for PortablePath {
             .map_err(|error| de::Error::custom(error.message))
     }
 }
+
+impl PartialEq<&str> for PortablePath {
+    fn eq(&self, other: &&str) -> bool {
+        match self {
+            PortablePath::EnvVar(var) => var == other,
+            PortablePath::ProjectFile(file) | PortablePath::WorkspaceFile(file) => file == other,
+            PortablePath::ProjectGlob(glob) | PortablePath::WorkspaceGlob(glob) => glob == other,
+        }
+    }
+}

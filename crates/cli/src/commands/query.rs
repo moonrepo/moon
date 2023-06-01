1e83081583542c7eb5c6a945fa580785e7957499
Miles Johnson <mileswjohnson@gmail.com>
2023-06-01 16:19:11 -0700

    Remove is terminal.

--- crates/cli/src/commands/query.rs
+++ crates/cli/src/commands/query.rs
@@ -7,14 +7,13 @@ pub use crate::queries::touched_files::{
     query_touched_files, QueryTouchedFilesOptions, QueryTouchedFilesResult,
 };
 use console::Term;
-use is_terminal::IsTerminal;
 use miette::IntoDiagnostic;
 use moon::load_workspace;
 use moon_terminal::ExtendedTerm;
 use rustc_hash::FxHashMap;
 use starbase::AppResult;
 use starbase_styles::color;
-use std::io;
+use std::io::{self, IsTerminal};
 
 pub async fn hash(hash: &str, json: bool) -> AppResult {
     let workspace = load_workspace().await?;

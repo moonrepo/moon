9423fc45d88cdf109851bde49e845bf1d5a1bb76
Miles Johnson <mileswjohnson@gmail.com>
2023-06-05 13:47:34 -0700

    Add config.

--- nextgen/config/src/project/owners_config.rs
+++ nextgen/config/src/project/owners_config.rs
@@ -1,6 +1,6 @@
 use moon_common::cacheable;
 use rustc_hash::FxHashMap;
-use schematic::{derive_enum, Config, ValidateError};
+use schematic::{derive_enum, Config, Segment, ValidateError};
 
 derive_enum!(
     #[serde(
@@ -19,9 +19,37 @@ impl Default for OwnersPaths {
     }
 }
 
-fn validate_required_approvals<D, C>(
+fn validate_paths<C>(
+    value: &OwnersPaths,
+    data: &PartialOwnersConfig,
+    _context: &C,
+) -> Result<(), ValidateError> {
+    match value {
+        OwnersPaths::List(list) => {
+            if !list.is_empty() && data.default_owner.is_none() {
+                return Err(ValidateError::new(
+                    "a default owner is required when defining a list of paths",
+                ));
+            }
+        }
+        OwnersPaths::Map(map) => {
+            for (key, value) in map {
+                if value.is_empty() && data.default_owner.is_none() {
+                    return Err(ValidateError::with_segment(
+                        "a default owner is required when defining an empty list of owners",
+                        Segment::Key(key.to_owned()),
+                    ));
+                }
+            }
+        }
+    };
+
+    Ok(())
+}
+
+fn validate_required_approvals<C>(
     value: &u8,
-    _data: &D,
+    _data: &PartialOwnersConfig,
     _context: &C,
 ) -> Result<(), ValidateError> {
     if *value <= 0 {
@@ -42,6 +70,7 @@ cacheable!(
         // GitLab
         pub optional: bool,
 
+        #[setting(validate = validate_paths)]
         pub paths: OwnersPaths,
 
         // GitLab

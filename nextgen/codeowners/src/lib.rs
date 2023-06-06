849e6fe6fee1a5880d56976ad762b69b2e17b212
Miles Johnson <mileswjohnson@gmail.com>
2023-06-06 10:04:41 -0700

    Implement generator.

--- nextgen/codeowners/src/lib.rs
+++ nextgen/codeowners/src/lib.rs
@@ -1,5 +1,5 @@
 use chrono::Local;
-use moon_config::{OwnersConfig, OwnersPaths, VcsProvider};
+use moon_config::{CodeownersConfig, OwnersConfig, OwnersPaths, VcsProvider};
 use starbase_utils::fs::{self, FsError};
 use std::fs::File;
 use std::io::Write;
@@ -41,7 +41,7 @@ impl CodeownersGenerator {
         self.write("")?;
 
         // Render the header
-        self.write(format!("# {} ({})", name, source))?;
+        self.write(format!("# {}", name))?;
 
         match &self.provider {
             VcsProvider::Bitbucket => {
@@ -76,13 +76,78 @@ impl CodeownersGenerator {
         };
 
         // Render the owner entries
-        self.render_entries(source, config)?;
+        let root = PathBuf::from("/").join(source);
+
+        match &config.paths {
+            OwnersPaths::List(paths) => {
+                for path in paths {
+                    if matches!(self.provider, VcsProvider::GitLab) {
+                        self.write(root.join(path).to_string_lossy())?;
+                    } else {
+                        self.write(format!(
+                            "{} {}",
+                            self.format_path(root.join(path)),
+                            config.default_owner.as_ref().unwrap()
+                        ))?;
+                    }
+                }
+            }
+            OwnersPaths::Map(map) => {
+                for (path, owners) in map {
+                    if owners.is_empty() {
+                        self.write(format!(
+                            "{} {}",
+                            self.format_path(root.join(path)),
+                            config.default_owner.as_ref().unwrap()
+                        ))?;
+                    } else {
+                        self.write(format!(
+                            "{} {}",
+                            self.format_path(root.join(path)),
+                            owners.join(" ")
+                        ))?;
+                    }
+                }
+            }
+        };
 
         Ok(())
     }
 
-    fn render_entries(&mut self, source: &str, config: &OwnersConfig) -> Result<(), FsError> {
-        todo!()
+    pub fn add_workspace_entries(&mut self, config: &CodeownersConfig) -> Result<(), FsError> {
+        if config.global_paths.is_empty() {
+            return Ok(());
+        }
+
+        self.write("")?;
+        self.write("# (workspace)")?;
+
+        for (path, owners) in &config.global_paths {
+            self.write(format!("{} {}", self.format_path(path), owners.join(" ")))?;
+        }
+
+        Ok(())
+    }
+
+    pub fn generate(mut self) -> Result<(), FsError> {
+        self.write("")?;
+
+        self.file.flush().map_err(|error| FsError::Create {
+            path: self.file_path.to_path_buf(),
+            error,
+        })?;
+
+        Ok(())
+    }
+
+    fn format_path<T: AsRef<Path>>(&self, path: T) -> String {
+        let path = path.as_ref().to_string_lossy();
+
+        if path.contains(' ') {
+            return path.replace(" ", "\\ ");
+        }
+
+        path.to_string()
     }
 
     fn write<T: AsRef<str>>(&mut self, message: T) -> Result<(), FsError> {
@@ -93,8 +158,4 @@ impl CodeownersGenerator {
 
         Ok(())
     }
-
-    // pub fn build(self) -> Codeowners {
-    //     Codeowners { file: self.file }
-    // }
 }
--- nextgen/config/src/workspace/codeowners_config.rs
+++ nextgen/config/src/workspace/codeowners_config.rs
@@ -1,3 +1,4 @@
+use rustc_hash::FxHashMap;
 use schematic::{derive_enum, Config, ConfigEnum};
 
 derive_enum!(
@@ -11,6 +12,8 @@ derive_enum!(
 
 #[derive(Config)]
 pub struct CodeownersConfig {
+    pub global_paths: FxHashMap<String, Vec<String>>,
+
     pub order_by: CodeownersOrderBy,
 
     pub sync_on_run: bool,

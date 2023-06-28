004141de52c386ab8e7a6bfe7f493dbe698c47ac
Miles Johnson <mileswjohnson@gmail.com>
2023-06-28 13:48:34 -0700

    Delete old generator.

--- crates/core/generator/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod errors;
-mod filters;
-mod generator;
-mod template;
-
-pub use errors::GeneratorError;
-pub use generator::Generator;
-pub use template::*;
-pub use tera::Context as TemplateContext;

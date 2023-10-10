2d17f5a52da45ffcb24f5a92e282b149e11ddc31
Miles Johnson <mileswjohnson@gmail.com>
2023-10-10 16:07:42 -0700

    Set nightly output.

--- scripts/release/planRelease.sh
+++ scripts/release/planRelease.sh
@@ -12,5 +12,8 @@ if [[ "$NIGHTLY" == "true" ]]; then
 	channel=nightly
 fi
 
+echo "Setting nightly to $NIGHTLY"
+echo "nightly=$NIGHTLY" >> "$GITHUB_OUTPUT"
+
 echo "Setting npm channel to $channel"
 echo "npm-channel=$channel" >> "$GITHUB_OUTPUT"

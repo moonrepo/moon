3e99e6829d8a299fa73adda683bf52a971d817f7
Miles Johnson <mileswjohnson@gmail.com>
2023-11-01 11:21:30 -0700

    Bump version.

--- crates/cli/tests/run_node_test.rs
+++ crates/cli/tests/run_node_test.rs
@@ -1048,8 +1048,6 @@ mod bun {
             cmd.arg("run").arg("bun:version");
         });
 
-        assert.debug();
-
         assert!(predicate::str::contains("1.0.0").eval(&assert.output()));
     }
 

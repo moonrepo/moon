fd7653108850c62645cae6136e1f02680d417145
Miles Johnson <mileswjohnson@gmail.com>
2023-11-19 16:58:23 -0800

    Create js platform.

--- crates/bun/platform/src/actions/sync_project.rs
+++ crates/bun/platform/src/actions/sync_project.rs
@@ -1,16 +1,12 @@
 use moon_common::Id;
-use moon_config::{BunConfig, DependencyScope, NodeVersionFormat, TypeScriptConfig};
+use moon_config::{BunConfig, DependencyScope, TypeScriptConfig};
+use moon_javascript_platform::JavaScriptSyncer;
 use moon_logger::debug;
-use moon_node_lang::{PackageJson, NPM};
 use moon_project::Project;
-use moon_utils::{path, semver};
 use rustc_hash::{FxHashMap, FxHashSet};
 use starbase_styles::color;
-use std::collections::BTreeMap;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
-
-const LOG_TARGET: &str = "moon:bun-platform:sync-project";
 
 pub async fn sync_project(
     project: &Project,
@@ -20,12 +16,14 @@ pub async fn sync_project(
     typescript_config: &Option<TypeScriptConfig>,
 ) -> miette::Result<bool> {
     let mut mutated_project_files = false;
+
+    if JavaScriptSyncer::for_bun(project, bun_config).sync(dependencies)? {
+        mutated_project_files = true;
+    }
+
     let is_project_typescript_enabled = project.config.toolchain.is_typescript_enabled();
 
     // Sync each dependency to `tsconfig.json` and `package.json`
-    let mut package_prod_deps: BTreeMap<String, String> = BTreeMap::new();
-    let mut package_peer_deps: BTreeMap<String, String> = BTreeMap::new();
-    let mut package_dev_deps: BTreeMap<String, String> = BTreeMap::new();
     let mut tsconfig_project_refs: FxHashSet<PathBuf> = FxHashSet::default();
 
     for (dep_id, dep_cfg) in &project.dependencies {
@@ -37,69 +35,7 @@ pub async fn sync_project(
             continue;
         }
 
-        let dep_relative_path =
-            path::relative_from(&dep_project.root, &project.root).unwrap_or_default();
         let is_dep_typescript_enabled = dep_project.config.toolchain.is_typescript_enabled();
-
-        // Update dependencies within this project's `package.json`.
-        // Only add if the dependent project has a `package.json`,
-        // and this `package.json` has not already declared the dep.
-        if bun_config.sync_project_workspace_dependencies && !project.is_root_level() {
-            let format = &bun_config.dependency_version_format;
-
-            if let Some(dep_package_json) = PackageJson::read(&dep_project.root)? {
-                if let Some(dep_package_name) = &dep_package_json.name {
-                    let version_prefix = format.get_prefix();
-                    let dep_package_version = dep_package_json.version.unwrap_or_default();
-                    let dep_version = match format {
-                        NodeVersionFormat::File | NodeVersionFormat::Link => {
-                            format!(
-                                "{}{}",
-                                version_prefix,
-                                path::to_virtual_string(&dep_relative_path)?
-                            )
-                        }
-                        NodeVersionFormat::Version
-                        | NodeVersionFormat::VersionCaret
-                        | NodeVersionFormat::VersionTilde => {
-                            format!("{version_prefix}{dep_package_version}")
-                        }
-                        _ => version_prefix,
-                    };
-
-                    match dep_cfg.scope {
-                        DependencyScope::Build | DependencyScope::Root => {
-                            // Not supported by Node.js
-                        }
-                        DependencyScope::Production => {
-                            package_prod_deps.insert(dep_package_name.to_owned(), dep_version);
-                        }
-                        DependencyScope::Development => {
-                            package_dev_deps.insert(dep_package_name.to_owned(), dep_version);
-                        }
-                        DependencyScope::Peer => {
-                            // Peers are unique, so lets handle this manually here for now.
-                            // Perhaps we can wrap this in a new setting in the future.
-                            package_peer_deps.insert(
-                                dep_package_name.to_owned(),
-                                format!(
-                                    "^{}.0.0",
-                                    semver::extract_major_version(&dep_package_version)
-                                ),
-                            );
-                        }
-                    }
-
-                    debug!(
-                        target: LOG_TARGET,
-                        "Syncing {} as a dependency to {}'s {}",
-                        color::id(&dep_project.id),
-                        color::id(&project.id),
-                        color::file(NPM.manifest)
-                    );
-                }
-            }
-        }
 
         if let Some(typescript_config) = &typescript_config {
             // Update `references` within this project's `tsconfig.json`.
@@ -115,7 +51,6 @@ pub async fn sync_project(
                 tsconfig_project_refs.insert(dep_project.root.clone());
 
                 debug!(
-                    target: LOG_TARGET,
                     "Syncing {} as a project reference to {}'s {}",
                     color::id(&dep_project.id),
                     color::id(&project.id),
@@ -123,40 +58,6 @@ pub async fn sync_project(
                 );
             }
         }
-    }
-
-    // Sync to the project's `package.json`
-    if !package_prod_deps.is_empty()
-        || !package_dev_deps.is_empty()
-        || !package_peer_deps.is_empty()
-    {
-        PackageJson::sync(&project.root, |package_json| {
-            let mut mutated_package = false;
-
-            for (name, version) in package_prod_deps {
-                if package_json.add_dependency(&name, &version, true) {
-                    mutated_package = true;
-                }
-            }
-
-            for (name, version) in package_dev_deps {
-                if package_json.add_dev_dependency(&name, &version, true) {
-                    mutated_package = true;
-                }
-            }
-
-            for (name, version) in package_peer_deps {
-                if package_json.add_peer_dependency(&name, &version, true) {
-                    mutated_package = true;
-                }
-            }
-
-            if mutated_package {
-                mutated_project_files = true;
-            }
-
-            Ok(mutated_package)
-        })?;
     }
 
     // Sync the project and root `tsconfig.json`

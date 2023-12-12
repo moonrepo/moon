faa988fc5d7588022c217797cf1e5d4bed8cb35d
Miles Johnson <milesj@users.noreply.github.com>
2023-12-11 19:23:37 -0800

    docs: Finalize blog post and docs. (#1226)
    * Fix codeowners.
    
    * Update init.
    
    * Start on toolchain.
    
    * Update proto.
    
    * Add new toolchain docs.
    
    * Update deps.
    
    * Dedupe.
    
    * Decline viz.
    
    * Fix format.
    
    * Improve logging.

--- nextgen/codeowners/src/codeowners_generator.rs
+++ nextgen/codeowners/src/codeowners_generator.rs
@@ -160,12 +160,6 @@ impl CodeownersGenerator {
     pub fn generate(mut self) -> miette::Result<()> {
         debug!(file = ?self.file_path, "Generating and writing CODEOWNERS file");
 
-        let editor_config = fs::get_editor_config_props(&self.file_path);
-
-        if editor_config.eof == "\n" {
-            self.write("")?;
-        }
-
         self.file.flush().map_err(|error| FsError::Create {
             path: self.file_path.to_path_buf(),
             error,

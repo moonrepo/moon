c5e6a50c620220135014c529f09898f3b482fe13
Miles Johnson <mileswjohnson@gmail.com>
2023-12-14 11:24:40 -0800

    Add dep.

--- crates/core/runner/src/errors.rs
+++ crates/core/runner/src/errors.rs
@@ -5,7 +5,7 @@ use thiserror::Error;
 
 #[derive(Error, Debug, Diagnostic)]
 pub enum RunnerError {
-    #[diagnostic(code(target_runner::run_failed))]
+    #[diagnostic(code(task_runner::run_failed))]
     #[error(
         "Task {} failed to run. View hash details with {}.",
         .target.style(Style::Label),
@@ -18,7 +18,7 @@ pub enum RunnerError {
         error: ProcessError,
     },
 
-    #[diagnostic(code(target_runner::missing_dep_hash))]
+    #[diagnostic(code(task_runner::missing_dep_hash))]
     #[error(
         "Encountered a missing hash for target {}, which is a dependency of {}.\nThis either means the dependency hasn't ran, has failed, or there's a misconfiguration.\n\nTry disabling the target's cache, or marking it as local.",
         .0.style(Style::Label),
@@ -26,7 +26,7 @@ pub enum RunnerError {
     )]
     MissingDependencyHash(String, String),
 
-    #[diagnostic(code(target_runner::missing_output))]
+    #[diagnostic(code(task_runner::missing_output))]
     #[error("Target {} defines outputs, but none exist after being ran.", .0.style(Style::Label))]
     MissingOutput(String),
 }

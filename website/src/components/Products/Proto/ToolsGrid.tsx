211b239f872fe0d02e4f8c5f1d0fe2d7983652a6
Miles Johnson <mileswjohnson@gmail.com>
2023-12-21 15:41:57 -0800

    Update docs.

--- website/src/components/Products/Proto/ToolsGrid.tsx
+++ website/src/components/Products/Proto/ToolsGrid.tsx
@@ -7,6 +7,7 @@ import Go from '@site/static/img/tools/go.svg';
 import Node from '@site/static/img/tools/node.svg';
 import Python from '@site/static/img/tools/python.svg';
 import Rust from '@site/static/img/tools/rust.svg';
+import { THIRD_PARTY_TOOLS } from '../../../data/proto-tools';
 
 export interface ToolsGridProps {
 	cols?: number;
@@ -14,45 +15,55 @@ export interface ToolsGridProps {
 
 export default function ToolsGrid({ cols = 3 }: ToolsGridProps) {
 	return (
-		<div className={clsx('grid gap-4 px-4', cols === 6 ? 'grid-cols-6' : 'grid-cols-3')}>
-			<div className="text-center">
-				<Link href="/docs/proto/tools#bun">
-					<Bun width="100%" className="inline-block" />
-				</Link>
+		<div>
+			<div className={clsx('grid gap-4 px-4', cols === 6 ? 'grid-cols-6' : 'grid-cols-3')}>
+				<div className="text-center">
+					<Link href="/docs/proto/tools#bun">
+						<Bun width="100%" className="inline-block" />
+					</Link>
+				</div>
+
+				<div className="text-center">
+					<Link href="/docs/proto/tools#deno">
+						<Deno width="100%" className="inline-block" />
+					</Link>
+				</div>
+
+				<div className="text-center">
+					<Link href="/docs/proto/tools#go">
+						<Go width="100%" className="inline-block" />
+					</Link>
+				</div>
+
+				<div className="text-center">
+					<Link href="/docs/proto/tools#node">
+						<Node width="100%" className="inline-block" />
+					</Link>
+
+					<Text className="m-0" variant="muted">
+						+ npm, pnpm, yarn
+					</Text>
+				</div>
+
+				<div className="text-center">
+					<Link href="/docs/proto/tools#python">
+						<Python width="100%" className="inline-block" />
+					</Link>
+				</div>
+
+				<div className="text-center">
+					<Link href="/docs/proto/tools#rust">
+						<Rust width="100%" className="inline-block" />
+					</Link>
+				</div>
 			</div>
 
-			<div className="text-center">
-				<Link href="/docs/proto/tools#deno">
-					<Deno width="100%" className="inline-block" />
-				</Link>
-			</div>
-
-			<div className="text-center">
-				<Link href="/docs/proto/tools#go">
-					<Go width="100%" className="inline-block" />
-				</Link>
-			</div>
-
-			<div className="text-center">
-				<Link href="/docs/proto/tools#node">
-					<Node width="100%" className="inline-block" />
-				</Link>
-
-				<Text className="m-0" variant="muted">
-					+ npm, pnpm, yarn
+			<div className="text-center mt-3">
+				<Text weight="bold" variant="muted">
+					<Link href="/docs/proto/tools#third-party">
+						...with {Object.keys(THIRD_PARTY_TOOLS).length} more and growing...
+					</Link>
 				</Text>
-			</div>
-
-			<div className="text-center">
-				<Link href="/docs/proto/tools#python">
-					<Python width="100%" className="inline-block" />
-				</Link>
-			</div>
-
-			<div className="text-center">
-				<Link href="/docs/proto/tools#rust">
-					<Rust width="100%" className="inline-block" />
-				</Link>
 			</div>
 		</div>
 	);

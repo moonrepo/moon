c1af55a8d4f1bbff2bfda1138725352f17a3270d
matt penrice <matthew.penrice@gmail.com>
2024-01-04 19:50:55 +0100

    Force arch when running in a bun postinstall process

--- packages/cli/postinstall.js
+++ packages/cli/postinstall.js
@@ -13,7 +13,8 @@ const isMacos = process.platform === 'darwin';
 const isWindows = process.platform === 'win32';
 
 const platform = isWindows ? 'windows' : isMacos ? 'macos' : process.platform;
-const parts = [platform, process.arch];
+const arch = process.env['npm_config_user_agent'].match(/^bun.*arm64$/) ? 'arm64' : process.arch; // https://github.com/moonrepo/moon/issues/1103
+const parts = [platform, arch];
 
 if (isLinux) {
 	const { familySync } = require('detect-libc');

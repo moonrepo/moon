00e9facd6f609da30e188019ae4000290290ef4e
Miles Johnson <milesj@users.noreply.github.com>
2024-01-10 17:41:26 -0800

    internal: Move proto/toolchain logic into systems. (#1267)
    * Add system>
    
    * Move to systems.
    
    * Fix systems.
    
    * Update changelog.
    
    * Fix format.

--- crates/cli/src/commands/setup.rs
+++ crates/cli/src/commands/setup.rs
@@ -1,12 +1,12 @@
 use crate::helpers::create_progress_bar;
-use moon::load_workspace_with_toolchain;
+use moon::load_toolchain;
 use starbase::system;
 
 #[system]
 pub async fn setup() {
     let done = create_progress_bar("Downloading and installing tools...");
 
-    load_workspace_with_toolchain().await?;
+    load_toolchain().await?;
 
     done("Setup complete", true);
 }

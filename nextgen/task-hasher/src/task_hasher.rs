9c6b4825e38eeff1d1b667c681b7fb96755c58a8
Miles Johnson <mileswjohnson@gmail.com>
2024-02-19 14:08:12 -0800

    Add more.

--- nextgen/task-hasher/src/task_hasher.rs
+++ nextgen/task-hasher/src/task_hasher.rs
@@ -1,41 +1,46 @@
-use moon_common::is_ci;
-use moon_common::path::WorkspaceRelativePathBuf;
+use miette::IntoDiagnostic;
+use moon_common::consts::CONFIG_PROJECT_FILENAME;
+use moon_common::path::{PathExt, WorkspaceRelativePath, WorkspaceRelativePathBuf};
+use moon_common::{color, is_ci};
 use moon_config::{HasherConfig, HasherWalkStrategy};
+use moon_project::Project;
 use moon_task::Task;
 use moon_vcs::BoxedVcs;
 use rustc_hash::FxHashSet;
-use starbase_utils::glob;
+use starbase_utils::glob::{self, GlobSet};
 use std::collections::BTreeMap;
 use std::path::{Path, PathBuf};
+use tracing::{debug, warn};
 
+// Hash all inputs for a task, but exclude outputs and moon specific configuration files!
 pub struct TaskHasher<'task> {
     pub hasher_config: &'task HasherConfig,
+    pub project: &'task Project,
     pub task: &'task Task,
-    pub project_root: &'task Path,
-    pub project_source: &'task str,
     pub vcs: &'task BoxedVcs,
     pub workspace_root: &'task Path,
 
-    files_to_hash: FxHashSet<PathBuf>,
+    aggregated_files: FxHashSet<PathBuf>,
+    processed_files: FxHashSet<WorkspaceRelativePathBuf>,
 }
 
 impl<'task> TaskHasher<'task> {
-    // Hash all inputs for a task, but exclude outputs  and moon specific configuration files!
     pub async fn aggregate_inputs(&mut self) -> miette::Result<()> {
+        let mut files = FxHashSet::default();
+
         if !self.task.input_files.is_empty() {
             for input in &self.task.input_files {
-                self.files_to_hash
-                    .insert(input.to_path(self.workspace_root));
+                files.insert(input.to_path(self.workspace_root));
             }
         }
 
         if !self.task.input_globs.is_empty() {
-            let use_globs = self.project_root == self.workspace_root
+            let use_globs = self.project.root == self.workspace_root
                 || matches!(self.hasher_config.walk_strategy, HasherWalkStrategy::Glob);
 
             // Collect inputs by walking and globbing the file system
             if use_globs {
-                self.files_to_hash.extend(glob::walk_files(
+                files.extend(glob::walk_files(
                     self.workspace_root,
                     &self.task.input_globs,
                 )?);
@@ -43,8 +48,8 @@ impl<'task> TaskHasher<'task> {
                 // Collect inputs by querying VCS
             } else {
                 // Using VCS to collect inputs in a project is faster than globbing
-                for file in self.vcs.get_file_tree(self.project_source).await? {
-                    self.files_to_hash.insert(file.to_path(self.workspace_root));
+                for file in self.vcs.get_file_tree(self.project.source.as_str()).await? {
+                    files.insert(file.to_path(self.workspace_root));
                 }
 
                 // However that completely ignores workspace level globs,
@@ -53,12 +58,11 @@ impl<'task> TaskHasher<'task> {
                     .task
                     .input_globs
                     .iter()
-                    .filter(|g| !g.starts_with(self.project_source))
+                    .filter(|g| !g.starts_with(self.project.source.as_str()))
                     .collect::<Vec<_>>();
 
                 if !workspace_globs.is_empty() {
-                    self.files_to_hash
-                        .extend(glob::walk_files(self.workspace_root, workspace_globs)?);
+                    files.extend(glob::walk_files(self.workspace_root, workspace_globs)?);
                 }
             }
         }
@@ -72,19 +76,117 @@ impl<'task> TaskHasher<'task> {
                 // Deleted files are listed in `git status` but are
                 // not valid inputs, so avoid hashing them!
                 if local_file.exists() {
-                    self.files_to_hash.insert(local_file);
+                    files.insert(local_file);
                 }
             }
         }
+
+        self.aggregated_files = files;
 
         Ok(())
     }
 
     pub fn process_files(&mut self) -> miette::Result<()> {
+        let mut files = FxHashSet::default();
+        let ignore = GlobSet::new(&self.hasher_config.ignore_patterns)?;
+        let ignore_missing = GlobSet::new(&self.hasher_config.ignore_missing_patterns)?;
+
+        for path in &self.aggregated_files {
+            // We need to use relative paths from the workspace root
+            // so that it works the same across all machines
+            let rel_path = path.relative_to(&self.workspace_root).into_diagnostic()?;
+
+            // `git hash-object` will fail if you pass an unknown file
+            if !path.exists() && self.hasher_config.warn_on_missing_inputs {
+                if self.hasher_config.ignore_missing_patterns.is_empty()
+                    || !ignore_missing.is_match(path)
+                {
+                    warn!(
+                        "Attempted to hash input {} but it does not exist, skipping",
+                        color::rel_path(&rel_path),
+                    );
+                }
+
+                continue;
+            }
+
+            if !path.is_file() {
+                warn!(
+                    "Attempted to hash input {} but only files can be hashed, try using a glob instead",
+                   color::rel_path(&rel_path),
+                );
+
+                continue;
+            }
+
+            if ignore.is_match(path) {
+                debug!(
+                    "Not hashing input {} as it matches an ignore pattern",
+                    color::rel_path(&rel_path),
+                );
+            } else {
+                files.insert(rel_path);
+            }
+        }
+
+        // Filter out unwanted files
+        if !files.is_empty() {
+            let globset = self.task.create_globset()?;
+
+            files.retain(|file| self.is_valid_input_source(&globset, file));
+        }
+
+        self.processed_files = files;
+
         Ok(())
     }
 
-    pub fn generate_hashes(self) -> miette::Result<BTreeMap<WorkspaceRelativePathBuf, String>> {
-        Ok(BTreeMap::default())
+    pub async fn generate_hashes(
+        self,
+    ) -> miette::Result<BTreeMap<WorkspaceRelativePathBuf, String>> {
+        let mut hashed_files = BTreeMap::default();
+
+        if !self.processed_files.is_empty() {
+            let files = self
+                .processed_files
+                .into_iter()
+                .map(|file| file.to_string())
+                .collect::<Vec<_>>();
+
+            hashed_files.extend(
+                self.vcs
+                    .get_file_hashes(&files, true, self.hasher_config.batch_size)
+                    .await?,
+            );
+        }
+
+        Ok(hashed_files)
+    }
+
+    fn is_valid_input_source(
+        &self,
+        sources_globset: &GlobSet,
+        workspace_relative_path: &WorkspaceRelativePath,
+    ) -> bool {
+        // Don't invalidate existing hashes when moon.yml changes
+        // as we already hash the contents of each task!
+        if workspace_relative_path.ends_with(CONFIG_PROJECT_FILENAME) {
+            return false;
+        }
+
+        // Remove outputs first
+        if sources_globset.is_negated(workspace_relative_path.as_str()) {
+            return false;
+        }
+
+        for output in &self.task.output_files {
+            if workspace_relative_path == output || workspace_relative_path.starts_with(output) {
+                return false;
+            }
+        }
+
+        // Filter inputs second
+        self.task.input_files.contains(workspace_relative_path)
+            || sources_globset.matches(workspace_relative_path.as_str())
     }
 }

3dc7663c11d060293317accab25d847a5951da7c
Miles Johnson <mileswjohnson@gmail.com>
2024-02-19 11:00:06 -0800

    Add more.

--- nextgen/task-hasher/src/task_hasher.rs
+++ nextgen/task-hasher/src/task_hasher.rs
@@ -1,23 +1,27 @@
+use moon_common::is_ci;
+use moon_common::path::WorkspaceRelativePathBuf;
 use moon_config::{HasherConfig, HasherWalkStrategy};
 use moon_task::Task;
 use moon_vcs::BoxedVcs;
 use rustc_hash::FxHashSet;
 use starbase_utils::glob;
+use std::collections::BTreeMap;
 use std::path::{Path, PathBuf};
 
 pub struct TaskHasher<'task> {
-    files_to_hash: FxHashSet<PathBuf>,
+    pub hasher_config: &'task HasherConfig,
+    pub task: &'task Task,
+    pub project_root: &'task Path,
+    pub project_source: &'task str,
+    pub vcs: &'task BoxedVcs,
+    pub workspace_root: &'task Path,
 
-    task: &'task Task,
-    project_root: &'task Path,
-    project_source: &'task str,
-    vcs: &'task BoxedVcs,
-    workspace_root: &'task Path,
+    files_to_hash: FxHashSet<PathBuf>,
 }
 
 impl<'task> TaskHasher<'task> {
     // Hash all inputs for a task, but exclude outputs  and moon specific configuration files!
-    pub async fn aggregate_inputs(&mut self, hasher_config: &HasherConfig) -> miette::Result<()> {
+    pub async fn aggregate_inputs(&mut self) -> miette::Result<()> {
         if !self.task.input_files.is_empty() {
             for input in &self.task.input_files {
                 self.files_to_hash
@@ -27,7 +31,7 @@ impl<'task> TaskHasher<'task> {
 
         if !self.task.input_globs.is_empty() {
             let use_globs = self.project_root == self.workspace_root
-                || matches!(hasher_config.walk_strategy, HasherWalkStrategy::Glob);
+                || matches!(self.hasher_config.walk_strategy, HasherWalkStrategy::Glob);
 
             // Collect inputs by walking and globbing the file system
             if use_globs {
@@ -59,6 +63,28 @@ impl<'task> TaskHasher<'task> {
             }
         }
 
+        // Include local file changes so that development builds work.
+        // Also run this LAST as it should take highest precedence!
+        if !is_ci() {
+            for local_file in self.vcs.get_touched_files().await?.all() {
+                let local_file = local_file.to_path(self.workspace_root);
+
+                // Deleted files are listed in `git status` but are
+                // not valid inputs, so avoid hashing them!
+                if local_file.exists() {
+                    self.files_to_hash.insert(local_file);
+                }
+            }
+        }
+
         Ok(())
+    }
+
+    pub fn process_files(&mut self) -> miette::Result<()> {
+        Ok(())
+    }
+
+    pub fn generate_hashes(self) -> miette::Result<BTreeMap<WorkspaceRelativePathBuf, String>> {
+        Ok(BTreeMap::default())
     }
 }

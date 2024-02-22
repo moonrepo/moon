a0f6ef58a06cc7fcbc5948ec100a1759a9e9c13f
Miles Johnson <mileswjohnson@gmail.com>
2024-02-22 11:58:19 -0800

    Add bin file.

--- packages/nx-compat/src/index.ts
+++ packages/nx-compat/src/index.ts
@@ -1,9 +1,1 @@
-import { execute } from './execute';
-
-const [, command, ...argv] = process.argv;
-
-if (command === 'execute') {
-	await execute(argv);
-} else {
-	throw new Error(`Unknown command \`${command}\`.`);
-}
+// Nothing!

c786bdab194bdb8cd9dd9327e5311c5f4c0fc5b4
Miles Johnson <milesj@users.noreply.github.com>
2024-03-07 17:47:21 -0800

    internal: Migrate to new tsconfig crate. (#1371)
    * Add dep.
    
    * Remove old tsconfig.
    
    * Update usages.
    
    * Update tests.
    
    * Update dep.
    
    * Fix test.
    
    * Fix issues.

--- crates/core/lang/src/config.rs
+++ crates/core/lang/src/config.rs
@@ -303,3 +303,178 @@ macro_rules! config_cache_container {
         }
     };
 }
+
+#[macro_export]
+macro_rules! config_cache_model {
+    ($container:ident, $struct:ident, $file:expr, $reader:ident) => {
+        config_cache_model!($container, $struct, $file, $reader, noop_write);
+    };
+    ($container:ident, $struct:ident, $file:expr, $reader:ident, $writer:path) => {
+        config_cache_model!($container, $struct, $file, $reader, $writer, cache_container);
+    };
+    ($container:ident, $struct:ident, $file:expr, $reader:ident, $writer:path, $namespace:ident) => {
+        mod $namespace {
+            use super::*;
+
+            pub fn load_config_internal(path: &Path) -> miette::Result<$struct> {
+                use moon_logger::trace;
+                use starbase_styles::color;
+
+                trace!(
+                    target: "moon:lang:config",
+                    "Loading {}",
+                    color::path(&path),
+                );
+
+                let cfg: $struct = $reader(&path)?;
+
+                Ok(cfg)
+            }
+
+            // This merely exists to create the global cache!
+            #[cached(sync_writes = true, result = true)]
+            pub fn load_config(path: PathBuf) -> miette::Result<$struct> {
+                load_config_internal(&path)
+            }
+        }
+
+        pub fn noop_write(_path: &Path, _file: &$struct) -> miette::Result<()> {
+            Ok(()) // Do nothing
+        }
+
+        #[derive(Default)]
+        pub struct $container {
+            pub data: $struct,
+            pub dirty: Vec<String>,
+            pub path: PathBuf,
+        }
+
+        impl $container {
+            /// Read the config file from the cache. If not cached and the file exists,
+            /// load it and store in the cache, otherwise return none.
+            pub fn read<P: AsRef<Path>>(path: P) -> miette::Result<Option<$container>> {
+                $container::read_with_name(path, $file)
+            }
+
+            /// Read the config file from the cache using the provided file name.
+            pub fn read_with_name<P, N>(path: P, name: N) -> miette::Result<Option<$container>>
+            where
+                P: AsRef<Path>,
+                N: AsRef<str>
+            {
+                let mut path = path.as_ref().to_path_buf();
+                let name = name.as_ref();
+
+                if !path.ends_with(name) {
+                    path = path.join(name);
+                }
+
+                if path.exists() {
+                    let data = $namespace::load_config(path.clone())?;
+
+                    Ok(Some($container {
+                        data,
+                        dirty: vec![],
+                        path,
+                    }))
+                } else {
+                    Ok(None)
+                }
+            }
+
+            /// If the file exists, load it from the file system, mutate it,
+            /// write it back to the file system and to the cache.
+            pub fn sync<P, F>(path: P, func: F) -> miette::Result<bool>
+            where
+                P: AsRef<Path>,
+                F: FnOnce(&mut $container) -> miette::Result<bool>
+            {
+                $container::sync_with_name(path, $file, func)
+            }
+
+            pub fn sync_with_name<P, N, F>(path: P, name: N, func: F) -> miette::Result<bool>
+            where
+                P: AsRef<Path>,
+                N: AsRef<str>,
+                F: FnOnce(&mut $container) -> miette::Result<bool>
+            {
+                use cached::Cached;
+                use moon_logger::trace;
+                use starbase_styles::color;
+
+                let mut path = path.as_ref().to_path_buf();
+                let name = name.as_ref();
+
+                if !path.ends_with(name) {
+                    path = path.join(name);
+                }
+
+                // Abort early and dont acquire a lock if the config doesnt exist
+                if !path.exists() {
+                    return Ok(false);
+                }
+
+                let mut cache = $namespace::LOAD_CONFIG.lock().unwrap();
+                let mut data: $struct;
+
+                if let Some(item) = cache.cache_get(&path) {
+                    data = item.clone();
+                } else {
+                    data = $namespace::load_config_internal(&path)?;
+                }
+
+                let mut model = $container {
+                    data,
+                    dirty: vec![],
+                    path,
+                };
+
+                if func(&mut model)? {
+                    trace!(
+                        target: "moon:lang:config",
+                        "Syncing {} with changes",
+                        color::path(&model.path),
+                    );
+
+                    // Write to the file system
+                    $writer(&model.path, &model)?;
+
+                    // And store in the cache
+                    cache.cache_set(model.path, model.data);
+
+                    return Ok(true);
+                }
+
+                Ok(false)
+            }
+
+            pub fn save(&mut self) -> miette::Result<()> {
+                use cached::Cached;
+                use moon_logger::trace;
+                use starbase_styles::color;
+
+                if self.dirty.is_empty() {
+                    return Ok(());
+                }
+
+                trace!(
+                    target: "moon:lang:config",
+                    "Writing {} to cache",
+                    color::path(&self.path),
+                );
+
+                // Write to the file system
+                $writer(&self.path, self)?;
+
+                // And store in the cache
+                let mut cache = $namespace::LOAD_CONFIG.lock().unwrap();
+
+                cache.cache_set(self.path.to_path_buf(), self.data.to_owned());
+
+                self.dirty.clear();
+
+                Ok(())
+            }
+        }
+    };
+}

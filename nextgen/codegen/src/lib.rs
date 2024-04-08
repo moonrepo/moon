c1efeeb849c6e48de456865d378ef4b389026094
Miles Johnson <milesj@users.noreply.github.com>
2024-04-08 10:52:42 -0700

    new: Add `moon templates` command. (#1419)
    * Start on command.
    
    * Add more info.
    
    * Update changelog.
    
    * Polish.

--- nextgen/codegen/src/lib.rs
+++ nextgen/codegen/src/lib.rs
@@ -5,10 +5,12 @@ mod filters;
 mod funcs;
 mod template;
 mod template_file;
+mod templates_command;
 
 pub use asset_file::*;
 pub use codegen::*;
 pub use codegen_error::*;
 pub use template::*;
 pub use template_file::*;
+pub use templates_command::*;
 pub use tera::Context as TemplateContext;

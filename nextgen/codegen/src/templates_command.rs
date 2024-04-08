ccf77edbd54cbe2c96c353194f4aeb321cb50d44
Miles Johnson <mileswjohnson@gmail.com>
2024-04-07 20:57:29 -0700

    Add more info.

--- nextgen/codegen/src/templates_command.rs
+++ nextgen/codegen/src/templates_command.rs
@@ -1,21 +1,60 @@
 use crate::codegen::CodeGenerator;
+use moon_common::color;
 use moon_console::Console;
 
 pub async fn templates_command(
     mut generator: CodeGenerator<'_>,
     console: &Console,
 ) -> miette::Result<()> {
-    let out = console.stdout();
-
     generator.load_templates().await?;
 
-    out.print_header("Templates")?;
+    let out = console.stdout();
 
     for template in generator.templates.values() {
-        out.print_entry_header(&template.config.title)?;
-        out.write_line(&template.config.description)?;
-        out.write_newline()?;
+        out.print_entry_header(&template.id)?;
+
+        out.write_line(format!(
+            "{} {} {}",
+            color::label(&template.config.title),
+            color::muted("-"),
+            template.config.description
+        ))?;
+
+        out.print_entry("Source location", color::path(&template.root))?;
+
+        if let Some(destination) = &template.config.destination {
+            out.print_entry("Default destination", color::file(destination))?;
+        }
+
+        if !template.config.extends.is_empty() {
+            out.print_entry(
+                "Extends from",
+                template
+                    .config
+                    .extends
+                    .iter()
+                    .map(|ext| color::id(ext))
+                    .collect::<Vec<_>>()
+                    .join(&color::muted(", ")),
+            )?;
+        }
+
+        if !template.config.variables.is_empty() {
+            out.print_entry(
+                "Supported variables",
+                template
+                    .config
+                    .variables
+                    .keys()
+                    .map(|ext| color::property(ext))
+                    .collect::<Vec<_>>()
+                    .join(&color::muted(", ")),
+            )?;
+        }
     }
+
+    out.write_newline()?;
+    out.flush()?;
 
     Ok(())
 }

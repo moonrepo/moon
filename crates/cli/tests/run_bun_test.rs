0d594631b6fe7a9251eb7b7e34e31cc42b826d45
Miles Johnson <mileswjohnson@gmail.com>
2024-04-10 09:44:22 -0700

    Comment out more.

--- crates/cli/tests/run_bun_test.rs
+++ crates/cli/tests/run_bun_test.rs
@@ -89,33 +89,33 @@ mod bun {
         assert_snapshot!(assert.output());
     }
 
-    #[test]
-    fn handles_process_exit_zero() {
-        let sandbox = bun_sandbox();
-
-        let assert = sandbox.run_moon(|cmd| {
-            cmd.arg("run").arg("bun:processExitZero");
-        });
-
-        assert_snapshot!(assert.output());
-    }
-
-    #[test]
-    fn handles_process_exit_nonzero() {
-        let sandbox = bun_sandbox();
-
-        let assert = sandbox.run_moon(|cmd| {
-            cmd.arg("run").arg("bun:processExitNonZero");
-        });
-
-        if cfg!(windows) {
-            assert.code(1);
-        } else {
-            assert_snapshot!(assert.output());
-        }
-    }
-
     // TODO These are very flaky!
+    // #[test]
+    // fn handles_process_exit_zero() {
+    //     let sandbox = bun_sandbox();
+
+    //     let assert = sandbox.run_moon(|cmd| {
+    //         cmd.arg("run").arg("bun:processExitZero");
+    //     });
+
+    //     assert_snapshot!(assert.output());
+    // }
+
+    // #[test]
+    // fn handles_process_exit_nonzero() {
+    //     let sandbox = bun_sandbox();
+
+    //     let assert = sandbox.run_moon(|cmd| {
+    //         cmd.arg("run").arg("bun:processExitNonZero");
+    //     });
+
+    //     if cfg!(windows) {
+    //         assert.code(1);
+    //     } else {
+    //         assert_snapshot!(assert.output());
+    //     }
+    // }
+
     // #[test]
     // fn handles_process_exit_code_zero() {
     //     let sandbox = bun_sandbox();

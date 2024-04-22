76a927ffeeaaa9a73a348c5f410e7823522a74b5
Miles Johnson <mileswjohnson@gmail.com>
2024-04-22 10:39:18 -0700

    Add new crate.

--- nextgen/action-context/src/lib.rs
+++ nextgen/action-context/src/lib.rs
@@ -1,12 +1,13 @@
-use clap::ValueEnum;
 use dashmap::DashMap;
 use moon_common::path::WorkspaceRelativePathBuf;
 use moon_target::{Target, TargetLocator};
-use rustc_hash::{FxHashMap, FxHashSet};
+use rustc_hash::FxHashSet;
 use serde::{Deserialize, Serialize};
 use std::{path::PathBuf, sync::Arc};
+use tokio::sync::Mutex;
 
-#[derive(ValueEnum, Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
 pub enum ProfileType {
     Cpu,
     Heap,
@@ -39,7 +40,7 @@ pub struct ActionContext {
     /// Active mutexes for tasks to acquire locks against.
     /// @mutable
     #[serde(skip)]
-    pub named_mutexes: DashMap<String, Arc<tokio::sync::Mutex<()>>>,
+    pub named_mutexes: DashMap<String, Arc<Mutex<()>>>,
 
     /// Additional arguments passed after `--` to passthrough.
     pub passthrough_args: Vec<String>,
@@ -52,7 +53,7 @@ pub struct ActionContext {
 
     /// The current state of running tasks (via their target).
     /// @mutable
-    pub target_states: FxHashMap<Target, TargetState>,
+    pub target_states: DashMap<Target, TargetState>,
 
     /// Files that have currently been touched.
     pub touched_files: FxHashSet<WorkspaceRelativePathBuf>,
@@ -62,6 +63,19 @@ pub struct ActionContext {
 }
 
 impl ActionContext {
+    pub fn get_or_create_mutex(&self, name: &str) -> Arc<Mutex<()>> {
+        if let Some(value) = self.named_mutexes.get(name) {
+            return Arc::clone(&value);
+        }
+
+        let mutex = Arc::new(Mutex::new(()));
+
+        self.named_mutexes
+            .insert(name.to_owned(), Arc::clone(&mutex));
+
+        return mutex;
+    }
+
     pub fn should_inherit_args<T: AsRef<Target>>(&self, target: T) -> bool {
         if self.passthrough_args.is_empty() {
             return false;

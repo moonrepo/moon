2f3870bde9ee02a23b717eb0bba865261ced89a2
Miles Johnson <mileswjohnson@gmail.com>
2024-07-11 11:41:33 -0700

    Fix wasm.

--- crates/cli/tests/docker_test.rs
+++ crates/cli/tests/docker_test.rs
@@ -432,6 +432,7 @@ mod prune {
 mod prune_node {
     use super::*;
 
+    #[cfg(not(linux))]
     #[test]
     fn focuses_for_npm() {
         let (workspace_config, toolchain_config, tasks_config) =

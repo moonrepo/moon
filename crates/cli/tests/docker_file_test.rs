2f500b6ce0b4a364ae33f6c114c371a8378d7b65
Miles Johnson <milesj@users.noreply.github.com>
2024-07-12 12:08:46 -0700

    internal: Start on new CLI test utils. (#1555)
    * Update docs.
    
    * Add docker file tests.
    
    * Fix docker builds.
    
    * Fix windows.

---
+++ crates/cli/tests/docker_file_test.rs
@@ -0,0 +1,144 @@
+use moon_test_utils2::create_moon_sandbox;
+use starbase_sandbox::predicates::prelude::*;
+use std::fs;
+
+mod dockerfile {
+    use super::*;
+
+    #[test]
+    fn errors_for_unknown_project() {
+        let sandbox = create_moon_sandbox("dockerfile");
+
+        let assert = sandbox.run_bin(|cmd| {
+            cmd.args(["docker", "file", "missing", "--defaults"]);
+        });
+
+        assert.inner.stderr(predicate::str::contains(
+            "No project has been configured with the name or alias missing.",
+        ));
+    }
+
+    #[test]
+    fn errors_for_unknown_build_task() {
+        let sandbox = create_moon_sandbox("dockerfile");
+
+        let assert = sandbox.run_bin(|cmd| {
+            cmd.args([
+                "docker",
+                "file",
+                "no-tasks",
+                "--defaults",
+                "--buildTask",
+                "missing",
+            ]);
+        });
+
+        assert.inner.stderr(predicate::str::contains(
+            "Unknown task missing for project no-tasks.",
+        ));
+    }
+
+    #[test]
+    fn errors_for_unknown_start_task() {
+        let sandbox = create_moon_sandbox("dockerfile");
+
+        let assert = sandbox.run_bin(|cmd| {
+            cmd.args([
+                "docker",
+                "file",
+                "no-tasks",
+                "--defaults",
+                "--startTask",
+                "missing",
+            ]);
+        });
+
+        assert.inner.stderr(predicate::str::contains(
+            "Unknown task missing for project no-tasks.",
+        ));
+    }
+
+    #[test]
+    fn can_use_defaults() {
+        let sandbox = create_moon_sandbox("dockerfile");
+
+        sandbox
+            .run_bin(|cmd| {
+                cmd.args(["docker", "file", "has-tasks", "--defaults"]);
+            })
+            .success();
+
+        assert!(sandbox.path().join("has-tasks/Dockerfile").exists());
+    }
+
+    #[test]
+    fn can_change_dest() {
+        let sandbox = create_moon_sandbox("dockerfile");
+
+        sandbox
+            .run_bin(|cmd| {
+                cmd.args([
+                    "docker",
+                    "file",
+                    "has-tasks",
+                    "./nested/Dockerfile.prod",
+                    "--defaults",
+                ]);
+            })
+            .success();
+
+        assert!(sandbox
+            .path()
+            .join("has-tasks/nested/Dockerfile.prod")
+            .exists());
+    }
+
+    #[test]
+    fn can_customize_with_args() {
+        let sandbox = create_moon_sandbox("dockerfile");
+
+        sandbox
+            .run_bin(|cmd| {
+                cmd.args([
+                    "docker",
+                    "file",
+                    "has-tasks",
+                    "--image",
+                    "node:latest",
+                    "--buildTask",
+                    "build",
+                    "--startTask",
+                    "start",
+                    "--no-prune",
+                    "--no-toolchain",
+                ]);
+            })
+            .success();
+
+        let file = fs::read_to_string(sandbox.path().join("has-tasks/Dockerfile")).unwrap();
+
+        assert!(file.contains("FROM node:latest"));
+        assert!(file.contains("moon run has-tasks:build"));
+        assert!(file.contains("moon run has-tasks:start"));
+        assert!(!file.contains("moon docker prune"));
+        assert!(file.contains("MOON_TOOLCHAIN_FORCE_GLOBALS=1"));
+    }
+
+    #[test]
+    fn uses_docker_config() {
+        let sandbox = create_moon_sandbox("dockerfile");
+
+        sandbox
+            .run_bin(|cmd| {
+                cmd.args(["docker", "file", "with-config", "--defaults"]);
+            })
+            .success();
+
+        let file = fs::read_to_string(sandbox.path().join("with-config/Dockerfile")).unwrap();
+
+        assert!(file.contains("FROM oven/bun:latest"));
+        assert!(file.contains("moon run with-config:compile"));
+        assert!(file.contains("moon run with-config:serve"));
+        assert!(file.contains("moon docker prune"));
+    }
+}
---
+++ crates/test-utils/src/sandbox.rs
@@ -0,0 +1,69 @@
+pub use starbase_sandbox::{create_temp_dir, Sandbox, SandboxAssert, SandboxSettings};
+use std::collections::HashMap;
+use std::ops::Deref;
+
+pub struct MoonSandbox {
+    pub sandbox: Sandbox,
+}
+
+impl MoonSandbox {
+    pub fn new(mut sandbox: Sandbox) -> Self {
+        apply_settings(&mut sandbox);
+        create_workspace_files(&sandbox);
+
+        Self { sandbox }
+    }
+}
+
+impl Deref for MoonSandbox {
+    type Target = Sandbox;
+
+    fn deref(&self) -> &Self::Target {
+        &self.sandbox
+    }
+}
+
+fn apply_settings(sandbox: &mut Sandbox) {
+    let moon_dir = sandbox.path().join(".moon");
+
+    let mut env = HashMap::new();
+    env.insert("RUST_BACKTRACE", "1");
+    env.insert("WASMTIME_BACKTRACE_DETAILS", "1");
+    env.insert("NO_COLOR", "1");
+    // Store plugins in the sandbox
+    env.insert("MOON_HOME", moon_dir.to_str().unwrap());
+    // env.insert("PROTO_HOME", path.join(".proto"));
+    // Let our code know we're running tests
+    env.insert("MOON_TEST", "true");
+    env.insert("STARBASE_TEST", "true");
+    // Hide install output as it disrupts testing snapshots
+    env.insert("MOON_TEST_HIDE_INSTALL_OUTPUT", "true");
+    // Standardize file system paths for testing snapshots
+    env.insert("MOON_TEST_STANDARDIZE_PATHS", "true");
+    // Enable logging for code coverage
+    env.insert("MOON_LOG", "trace");
+    // Advanced debugging
+    // env.insert("PROTO_LOG", "trace");
+    // env.insert("MOON_DEBUG_WASM", "true");
+
+    sandbox.settings.bin = "moon".into();
+
+    sandbox
+        .settings
+        .env
+        .extend(env.into_iter().map(|(k, v)| (k.to_owned(), v.to_owned())));
+}
+
+fn create_workspace_files(sandbox: &Sandbox) {
+    if !sandbox.path().join(".moon/workspace.yml").exists() {
+        sandbox.create_file(".moon/workspace.yml", "projects: ['*']");
+    }
+}
+
+pub fn create_empty_moon_sandbox() -> MoonSandbox {
+    MoonSandbox::new(starbase_sandbox::create_empty_sandbox())
+}
+
+pub fn create_moon_sandbox<N: AsRef<str>>(fixture: N) -> MoonSandbox {
+    MoonSandbox::new(starbase_sandbox::create_sandbox(fixture))
+}

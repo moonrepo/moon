6a7da1b260833ce02a79ac324a969a7b8e9387c3
Miles Johnson <milesj@users.noreply.github.com>
2024-08-04 16:44:42 -0700

    new: Replace old pipeline with new pipeline. (#1600)
    * Remove old crates.
    
    * Update callsites.
    
    * Rename crate.
    
    * Resolve experiments.
    
    * Remove CI insights.
    
    * Update versions.
    
    * Fix tests.

--- crates/app/src/components.rs
+++ crates/app/src/components.rs
@@ -1,4 +1,9 @@
+use crate::app::Commands;
 use crate::session::CliSession;
+use moon_action::Action;
+use moon_action_context::ActionContext;
+use moon_action_graph::ActionGraph;
+use moon_action_pipeline::ActionPipeline;
 use moon_platform::PlatformManager;
 use moon_project_graph::{
     ExtendProjectData, ExtendProjectEvent, ExtendProjectGraphData, ExtendProjectGraphEvent,
@@ -7,6 +12,41 @@ use moon_project_graph::{
 use starbase_events::{Emitter, EventState};
 use std::sync::Arc;
 use tokio::sync::RwLock;
+
+pub async fn run_action_pipeline(
+    session: &CliSession,
+    action_context: ActionContext,
+    action_graph: ActionGraph,
+) -> miette::Result<Vec<Action>> {
+    let project_graph = session.get_project_graph().await?;
+    let mut pipeline = ActionPipeline::new(session.get_app_context()?, project_graph);
+
+    if let Some(concurrency) = &session.cli.concurrency {
+        pipeline.concurrency = *concurrency;
+    }
+
+    match &session.cli.command {
+        Commands::Check(cmd) => {
+            pipeline.bail = true;
+            pipeline.summarize = cmd.summary;
+        }
+        Commands::Ci(_) => {
+            pipeline.report_name = "ciReport.json".into();
+            pipeline.summarize = true;
+        }
+        Commands::Run(cmd) => {
+            pipeline.bail = true;
+            pipeline.summarize = cmd.summary;
+        }
+        _ => {}
+    };
+
+    let results = pipeline
+        .run_with_context(action_graph, action_context)
+        .await?;
+
+    Ok(results)
+}
 
 pub async fn create_project_graph_context(
     session: &CliSession,

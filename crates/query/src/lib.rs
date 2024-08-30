af51550ccbc571fe51d86bde46e4c2a248ca4d99
Miles Johnson <mileswjohnson@gmail.com>
2024-08-30 14:45:09 -0700

    Decouple project and query.

--- crates/query/src/lib.rs
+++ crates/query/src/lib.rs
@@ -5,7 +5,3 @@ mod query_error;
 pub use builder::*;
 pub use parser::*;
 pub use query_error::QueryError;
-
-pub trait Queryable {
-    fn matches_criteria(&self, criteria: &Criteria) -> miette::Result<bool>;
-}

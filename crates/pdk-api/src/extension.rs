3cb5ab969b310c973def6f853e49289bfc835de5
Miles Johnson <mileswjohnson@gmail.com>
2024-08-30 15:16:22 -0700

    Add metadata.

--- crates/pdk-api/src/extension.rs
+++ crates/pdk-api/src/extension.rs
@@ -1,5 +1,36 @@
 use crate::common::MoonContext;
+use schematic::Schema;
 use warpgate_api::*;
+
+// METADATA
+
+api_struct!(
+    /// Input passed to the `register_extension` function.
+    pub struct ExtensionMetadataInput {
+        /// ID of the toolchain, as it was configured.
+        pub id: String,
+    }
+);
+
+api_struct!(
+    /// Output returned from the `register_extension` function.
+    pub struct ExtensionMetadataOutput {
+        /// Schema shape of the tool's configuration.
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        pub config_schema: Option<Schema>,
+
+        /// Name of the extension.
+        pub name: String,
+
+        /// Optional description about what the extension does.
+        pub description: Option<String>,
+
+        /// Version of the plugin.
+        pub plugin_version: String,
+    }
+);
+
+// EXECUTE
 
 api_struct!(
     /// Input passed to the `execute_extension` function.

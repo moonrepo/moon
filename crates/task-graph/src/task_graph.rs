f1b4eb412877f80df32d0d8f07b4907aae22609c
Miles Johnson <mileswjohnson@gmail.com>
2024-11-06 17:45:53 -0800

    Add utils to tg.

--- crates/task-graph/src/task_graph.rs
+++ crates/task-graph/src/task_graph.rs
@@ -1,27 +1,17 @@
 use moon_config::DependencyType;
+use moon_graph_utils::*;
 use moon_target::Target;
 use moon_task::Task;
-use petgraph::dot::{Config, Dot};
 use petgraph::graph::{DiGraph, NodeIndex};
-use petgraph::visit::EdgeRef;
-use petgraph::Direction;
 use rustc_hash::FxHashMap;
-use serde::Serialize;
-use starbase_utils::json;
 use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
 use tracing::debug;
 
-pub type TaskGraphType = DiGraph<Target, DependencyType>;
+pub type TaskGraphType = DiGraph<Task, DependencyType>;
 pub type TasksCache = FxHashMap<Target, Arc<Task>>;
 
-#[derive(Serialize)]
-pub struct TaskGraphCache<'graph> {
-    graph: &'graph TaskGraphType,
-    tasks: &'graph TasksCache,
-}
-
 #[derive(Clone, Debug, Default)]
-pub struct TaskNode {
+pub struct TaskMetadata {
     pub index: NodeIndex,
 }
 
@@ -30,97 +20,22 @@ pub struct TaskGraph {
     /// Directed-acyclic graph (DAG) of non-expanded tasks and their relationships.
     graph: TaskGraphType,
 
-    /// Graph node information, mapped by target.
-    nodes: FxHashMap<Target, TaskNode>,
+    /// Task metadata, mapped by target.
+    metadata: FxHashMap<Target, TaskMetadata>,
 
     /// Expanded tasks, mapped by target.
     tasks: Arc<RwLock<TasksCache>>,
 }
 
 impl TaskGraph {
-    pub fn new(graph: TaskGraphType, nodes: FxHashMap<Target, TaskNode>) -> Self {
+    pub fn new(graph: TaskGraphType, metadata: FxHashMap<Target, TaskMetadata>) -> Self {
         debug!("Creating task graph");
 
         Self {
             graph,
-            nodes,
+            metadata,
             tasks: Arc::new(RwLock::new(FxHashMap::default())),
         }
-    }
-
-    pub fn dependencies_of(&self, task: &Task) -> miette::Result<Vec<&Target>> {
-        let deps = self
-            .graph
-            .neighbors_directed(
-                self.nodes.get(&task.target).unwrap().index,
-                Direction::Outgoing,
-            )
-            .map(|idx| self.graph.node_weight(idx).unwrap())
-            .collect();
-
-        Ok(deps)
-    }
-
-    pub fn dependents_of(&self, task: &Task) -> miette::Result<Vec<&Target>> {
-        let deps = self
-            .graph
-            .neighbors_directed(
-                self.nodes.get(&task.target).unwrap().index,
-                Direction::Incoming,
-            )
-            .map(|idx| self.graph.node_weight(idx).unwrap())
-            .collect();
-
-        Ok(deps)
-    }
-
-    /// Return a list of targets for all tasks currently within the graph.
-    pub fn targets(&self) -> Vec<&Target> {
-        self.graph.raw_nodes().iter().map(|n| &n.weight).collect()
-    }
-
-    /// Get a labelled representation of the graph (which can be serialized easily).
-    pub fn labeled_graph(&self) -> DiGraph<String, DependencyType> {
-        self.graph.map(|_, n| n.to_string(), |_, e| *e)
-    }
-
-    /// Format graph as a DOT string.
-    pub fn to_dot(&self) -> String {
-        let dot = Dot::with_attr_getters(
-            &self.graph,
-            &[Config::EdgeNoLabel, Config::NodeNoLabel],
-            &|_, e| {
-                let label = e.weight().to_string();
-
-                if e.source().index() == 0 {
-                    format!("label=\"{label}\" arrowhead=none")
-                } else {
-                    format!("label=\"{label}\" arrowhead=box, arrowtail=box")
-                }
-            },
-            &|_, n| {
-                let label = &n.1.id;
-
-                format!(
-                    "label=\"{label}\" style=filled, shape=oval, fillcolor=gray, fontcolor=black"
-                )
-            },
-        );
-
-        format!("{dot:?}")
-    }
-
-    /// Format graph as a JSON string.
-    pub fn to_json(&self) -> miette::Result<String> {
-        let tasks = self.read_cache();
-
-        Ok(json::format(
-            &TaskGraphCache {
-                graph: &self.graph,
-                tasks: &tasks,
-            },
-            true,
-        )?)
     }
 
     fn read_cache(&self) -> RwLockReadGuard<TasksCache> {
@@ -135,3 +50,25 @@ impl TaskGraph {
             .expect("Failed to acquire write access to task graph!")
     }
 }
+
+impl GraphData<Task, DependencyType, Target> for TaskGraph {
+    fn get_graph(&self) -> &DiGraph<Task, DependencyType> {
+        &self.graph
+    }
+
+    fn get_node_index(&self, node: &Task) -> NodeIndex {
+        self.metadata.get(&node.target).unwrap().index
+    }
+
+    fn get_node_key(&self, node: &Task) -> Target {
+        node.target.clone()
+    }
+}
+
+impl GraphConnections<Task, DependencyType, Target> for TaskGraph {}
+
+impl GraphConversions<Task, DependencyType, Target> for TaskGraph {}
+
+impl GraphToDot<Task, DependencyType, Target> for TaskGraph {}
+
+impl GraphToJson<Task, DependencyType, Target> for TaskGraph {}

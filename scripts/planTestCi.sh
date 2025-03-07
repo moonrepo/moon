cc2b4388ee0073ccbce6e0f06ae450c95b9fcf4d
Miles Johnson <mileswjohnson@gmail.com>
2025-03-07 10:50:57 -0800

    Update plan.

--- scripts/planTestCi.sh
+++ scripts/planTestCi.sh
@@ -2,18 +2,16 @@
 
 coverage="$COVERAGE"
 
-# Windows runs out of disk space
-if [[ "$OS" == "Windows" ]]; then
-	coverage="false"
-fi
-
+echo "coverage=$coverage"
 echo "coverage=$coverage" >> "$GITHUB_OUTPUT"
 
 osCoverage='["depot-ubuntu-22.04-4","macos-latest","windows-latest"]'
 os='["ubuntu-latest","macos-latest","windows-latest"]'
 
 if [[ "$coverage" == "true" ]]; then
-	echo "os='${osCoverage}'" >> "$GITHUB_OUTPUT"
+	echo "os=${osCoverage}"
+	echo "os=${osCoverage}" >> "$GITHUB_OUTPUT"
 else
-	echo "os='${os}'" >> "$GITHUB_OUTPUT"
+	echo "os=${os}"
+	echo "os=${os}" >> "$GITHUB_OUTPUT"
 fi

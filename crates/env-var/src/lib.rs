89832acdddaf15c29961bc303828bf65e611b963
Miles Johnson <mileswjohnson@gmail.com>
2025-03-13 09:12:41 -0700

    Add env crate.

--- crates/env-var/src/lib.rs
+++ crates/env-var/src/lib.rs
@@ -1,7 +1,13 @@
-use moon_config::patterns;
+use regex::Regex;
 use rustc_hash::{FxHashMap, FxHashSet};
 use std::env;
+use std::sync::LazyLock;
 use tracing::debug;
+
+// $ENV_VAR, ${ENV_VAR}
+pub static ENV_VAR_SUBSTITUTE: LazyLock<Regex> = LazyLock::new(|| {
+    Regex::new("(?:\\$\\{(?P<name1>[A-Z0-9_]+)(?P<flag1>[!?]{1})?\\})|(?:\\$(?P<name2>[A-Z0-9_]+)(?P<flag2>[!?]{1})?)").unwrap()
+});
 
 pub fn substitute_env_vars(mut env: FxHashMap<String, String>) -> FxHashMap<String, String> {
     let cloned_env = env.clone();
@@ -24,9 +30,9 @@ pub fn substitute_env_var(
         return value.to_owned();
     }
 
-    patterns::ENV_VAR_SUBSTITUTE.replace_all(
+    ENV_VAR_SUBSTITUTE.replace_all(
         value,
-        |caps: &patterns::Captures| {
+        |caps: &regex::Captures| {
             let Some(name) = caps.name("name1")
                 .or_else(|| caps.name("name2"))
                 .map(|cap| cap.as_str())
--- crates/task-expander/src/lib.rs
+++ crates/task-expander/src/lib.rs
@@ -1,10 +1,8 @@
-mod expander_utils;
 mod task_expander;
 mod task_expander_error;
 mod token_expander;
 mod token_expander_error;
 
-pub use expander_utils::*;
 pub use task_expander::*;
 pub use task_expander_error::*;
 pub use token_expander::*;

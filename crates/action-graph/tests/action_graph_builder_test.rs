1aca4a8c9d7eb834855b066c6ff461d7e68e2264
Miles Johnson <mileswjohnson@gmail.com>
2025-04-08 08:34:18 -0700

    Add sync project.

--- crates/action-graph/tests/action_graph_builder_test.rs
+++ crates/action-graph/tests/action_graph_builder_test.rs
@@ -1,7 +1,9 @@
-use moon_action::ActionNode;
+use moon_action::*;
 use moon_action_graph::{ActionGraph, action_graph_builder2::*};
 use moon_app_context::AppContext;
-use moon_test_utils2::{AppContextMocker, WorkspaceGraph};
+use moon_common::Id;
+use moon_config::PipelineActionSwitch;
+use moon_test_utils2::{AppContextMocker, WorkspaceGraph, generate_workspace_graph};
 use starbase_sandbox::{assert_snapshot, create_empty_sandbox};
 use std::sync::Arc;
 
@@ -9,8 +11,8 @@ fn mock_app_context() -> Arc<AppContext> {
     Arc::new(AppContextMocker::new(create_empty_sandbox().path()).mock())
 }
 
-fn mock_workspace_graph() -> Arc<WorkspaceGraph> {
-    Default::default()
+async fn mock_workspace_graph() -> Arc<WorkspaceGraph> {
+    Arc::new(generate_workspace_graph("projects").await)
 }
 
 fn topo(graph: ActionGraph) -> Vec<ActionNode> {
@@ -26,17 +28,217 @@ fn topo(graph: ActionGraph) -> Vec<ActionNode> {
 mod action_graph_builder {
     use super::*;
 
+    mod sync_project {
+        use super::*;
+
+        #[tokio::test]
+        async fn graphs_single() {
+            let wg = mock_workspace_graph().await;
+            let mut builder =
+                ActionGraphBuilder::new(mock_app_context(), wg.clone(), Default::default())
+                    .unwrap();
+
+            let bar = wg.get_project("bar").unwrap();
+            builder.sync_project(&bar).await.unwrap();
+
+            let graph = builder.build();
+
+            assert_snapshot!(graph.to_dot());
+            assert_eq!(
+                topo(graph),
+                vec![
+                    ActionNode::sync_workspace(),
+                    ActionNode::sync_project(SyncProjectNode {
+                        project_id: Id::raw("bar"),
+                    })
+                ]
+            );
+        }
+
+        #[tokio::test]
+        async fn graphs_multiple() {
+            let wg = mock_workspace_graph().await;
+            let mut builder =
+                ActionGraphBuilder::new(mock_app_context(), wg.clone(), Default::default())
+                    .unwrap();
+
+            let foo = wg.get_project("foo").unwrap();
+            builder.sync_project(&foo).await.unwrap();
+
+            let bar = wg.get_project("bar").unwrap();
+            builder.sync_project(&bar).await.unwrap();
+
+            let qux = wg.get_project("qux").unwrap();
+            builder.sync_project(&qux).await.unwrap();
+
+            let graph = builder.build();
+
+            assert_snapshot!(graph.to_dot());
+            assert_eq!(
+                topo(graph),
+                vec![
+                    ActionNode::sync_workspace(),
+                    ActionNode::sync_project(SyncProjectNode {
+                        project_id: Id::raw("bar"),
+                    }),
+                    ActionNode::sync_project(SyncProjectNode {
+                        project_id: Id::raw("foo"),
+                    }),
+                    ActionNode::sync_project(SyncProjectNode {
+                        project_id: Id::raw("qux"),
+                    }),
+                ]
+            );
+        }
+
+        #[tokio::test]
+        async fn graphs_without_deps() {
+            let wg = mock_workspace_graph().await;
+            let mut builder = ActionGraphBuilder::new(
+                mock_app_context(),
+                wg.clone(),
+                ActionGraphBuilderOptions {
+                    sync_project_dependencies: false,
+                    ..Default::default()
+                },
+            )
+            .unwrap();
+
+            let foo = wg.get_project("foo").unwrap();
+            builder.sync_project(&foo).await.unwrap();
+
+            let qux = wg.get_project("qux").unwrap();
+            builder.sync_project(&qux).await.unwrap();
+
+            let graph = builder.build();
+
+            assert_snapshot!(graph.to_dot());
+            assert_eq!(
+                topo(graph),
+                vec![
+                    ActionNode::sync_workspace(),
+                    ActionNode::sync_project(SyncProjectNode {
+                        project_id: Id::raw("foo"),
+                    }),
+                    ActionNode::sync_project(SyncProjectNode {
+                        project_id: Id::raw("qux"),
+                    }),
+                ]
+            );
+        }
+
+        #[tokio::test]
+        async fn ignores_dupes() {
+            let wg = mock_workspace_graph().await;
+            let mut builder =
+                ActionGraphBuilder::new(mock_app_context(), wg.clone(), Default::default())
+                    .unwrap();
+
+            let foo = wg.get_project("foo").unwrap();
+
+            builder.sync_project(&foo).await.unwrap();
+            builder.sync_project(&foo).await.unwrap();
+            builder.sync_project(&foo).await.unwrap();
+
+            let graph = builder.build();
+
+            assert_eq!(
+                topo(graph),
+                vec![
+                    ActionNode::sync_workspace(),
+                    ActionNode::sync_project(SyncProjectNode {
+                        project_id: Id::raw("bar"),
+                    }),
+                    ActionNode::sync_project(SyncProjectNode {
+                        project_id: Id::raw("foo"),
+                    })
+                ]
+            );
+        }
+
+        #[tokio::test]
+        async fn doesnt_add_if_disabled() {
+            let wg = mock_workspace_graph().await;
+            let mut builder = ActionGraphBuilder::new(
+                mock_app_context(),
+                wg.clone(),
+                ActionGraphBuilderOptions {
+                    sync_projects: false.into(),
+                    ..Default::default()
+                },
+            )
+            .unwrap();
+
+            let bar = wg.get_project("bar").unwrap();
+            builder.sync_project(&bar).await.unwrap();
+
+            let graph = builder.build();
+
+            assert_snapshot!(graph.to_dot());
+            assert_eq!(topo(graph), vec![]);
+        }
+
+        #[tokio::test]
+        async fn doesnt_add_if_not_listed() {
+            let wg = mock_workspace_graph().await;
+            let mut builder = ActionGraphBuilder::new(
+                mock_app_context(),
+                wg.clone(),
+                ActionGraphBuilderOptions {
+                    sync_projects: PipelineActionSwitch::Only(vec![Id::raw("foo")]),
+                    ..Default::default()
+                },
+            )
+            .unwrap();
+
+            let bar = wg.get_project("bar").unwrap();
+            builder.sync_project(&bar).await.unwrap();
+
+            let graph = builder.build();
+
+            assert_snapshot!(graph.to_dot());
+            assert_eq!(topo(graph), vec![]);
+        }
+
+        #[tokio::test]
+        async fn adds_if_listed() {
+            let wg = mock_workspace_graph().await;
+            let mut builder = ActionGraphBuilder::new(
+                mock_app_context(),
+                wg.clone(),
+                ActionGraphBuilderOptions {
+                    sync_projects: PipelineActionSwitch::Only(vec![Id::raw("bar")]),
+                    ..Default::default()
+                },
+            )
+            .unwrap();
+
+            let bar = wg.get_project("bar").unwrap();
+            builder.sync_project(&bar).await.unwrap();
+
+            let graph = builder.build();
+
+            assert_snapshot!(graph.to_dot());
+            assert_eq!(
+                topo(graph),
+                vec![
+                    ActionNode::sync_workspace(),
+                    ActionNode::sync_project(SyncProjectNode {
+                        project_id: Id::raw("bar"),
+                    })
+                ]
+            );
+        }
+    }
+
     mod sync_workspace {
         use super::*;
 
         #[tokio::test]
         async fn graphs() {
-            let mut builder = ActionGraphBuilder::new(
-                mock_app_context(),
-                mock_workspace_graph(),
-                Default::default(),
-            )
-            .unwrap();
+            let mut builder =
+                ActionGraphBuilder::new(mock_app_context(), Default::default(), Default::default())
+                    .unwrap();
 
             builder.sync_workspace().await;
 
@@ -48,12 +250,9 @@ mod action_graph_builder {
 
         #[tokio::test]
         async fn ignores_dupes() {
-            let mut builder = ActionGraphBuilder::new(
-                mock_app_context(),
-                mock_workspace_graph(),
-                Default::default(),
-            )
-            .unwrap();
+            let mut builder =
+                ActionGraphBuilder::new(mock_app_context(), Default::default(), Default::default())
+                    .unwrap();
 
             builder.sync_workspace().await;
             builder.sync_workspace().await;
@@ -68,7 +267,7 @@ mod action_graph_builder {
         async fn doesnt_add_if_disabled() {
             let mut builder = ActionGraphBuilder::new(
                 mock_app_context(),
-                mock_workspace_graph(),
+                Default::default(),
                 ActionGraphBuilderOptions {
                     sync_workspace: false,
                     ..Default::default()

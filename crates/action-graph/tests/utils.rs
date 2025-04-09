e9b72ae3c467dbe420df1bf8dcc30c07bfada622
Miles Johnson <mileswjohnson@gmail.com>
2025-04-09 10:44:55 -0700

    Fix tests.

--- crates/action-graph/tests/utils.rs
+++ crates/action-graph/tests/utils.rs
@@ -27,7 +27,9 @@ impl ActionGraphContainer {
 
     pub fn create_builder(&self) -> ActionGraphBuilder {
         let config = &self.workspace_config.pipeline;
-        let app_context = WorkspaceMocker::new(&self.workspace_root).mock_app_context();
+        let app_context = WorkspaceMocker::new(&self.workspace_root)
+            .with_all_toolchains()
+            .mock_app_context();
 
         ActionGraphBuilder::with_platforms(
             &self.platform_manager,

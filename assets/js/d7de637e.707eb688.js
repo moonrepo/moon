266f08a1564659bcc96782bf6ea6d2076d9f64ba
milesj <milesj@users.noreply.github.com>
2025-06-04 00:09:19 +0000

    deploy: 85583a68f34317d36208801239bef278e83eacca

---
+++ assets/js/d7de637e.707eb688.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[41072],{55084:t=>{t.exports=JSON.parse('{"tag":{"label":"rust","permalink":"/blog/tags/rust","allTagsPath":"/blog/tags","count":4,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/rust","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);

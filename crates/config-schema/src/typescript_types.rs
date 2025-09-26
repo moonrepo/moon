63ba3ebd10b84c414db3ecf79ea5954b581edefd
Miles Johnson <mileswjohnson@gmail.com>
2025-09-26 13:50:27 -0700

    Regen schemas.

--- crates/config-schema/src/typescript_types.rs
+++ crates/config-schema/src/typescript_types.rs
@@ -8,8 +8,8 @@ use std::path::Path;
 
 fn generate_project(out_dir: &Path) -> miette::Result<()> {
     let mut generator = SchemaGenerator::default();
-    generator.add::<DependencyType>();
-    generator.add::<DependencyConfig>();
+    generator.add::<TaskDependencyType>();
+    generator.add::<ProjectDependencyConfig>();
     generator.add::<ProjectConfig>();
     generator.add::<PartialProjectConfig>();
     generator.generate(

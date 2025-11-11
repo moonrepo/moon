51be7203b8e69ba8b60ff4e8dd3904c17645e589
Miles Johnson <mileswjohnson@gmail.com>
2025-11-10 20:57:10 -0800

    Add partition code.

--- crates/graph-utils/src/lib.rs
+++ crates/graph-utils/src/lib.rs
@@ -1,7 +1,9 @@
+mod dag_partition;
 mod graph_context;
 mod graph_formats;
 mod graph_traits;
 
+pub use dag_partition::*;
 pub use graph_context::*;
 pub use graph_formats::*;
 pub use graph_traits::*;

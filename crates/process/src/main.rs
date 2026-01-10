fbe48cfb48ee9821710cf0bc1ba377178e534717
Miles Johnson <mileswjohnson@gmail.com>
2026-01-10 13:26:18 -0800

    Add windows.

--- crates/process/src/main.rs
+++ crates/process/src/main.rs
@@ -1,6 +1,19 @@
 use std::process::Command;
+use std::env;
 
 fn main() {
+    let (shell, ext) = if cfg!(windows) {
+        ("pwsh", "ps1")
+    } else {
+        ("bash", "sh")
+    };
+    let script = format!("./crates/process/args.{ext}");
+
+
+    let mut paths = env::split_paths(&env::var_os("PATH").unwrap()).collect::<Vec<_>>();
+    paths.push(env::current_dir().unwrap());
+    let paths = env::join_paths(paths).unwrap();
+
     // Unix:
     //	Quotes are preserved in strings, so they're passed as ""double quote"".
     //
@@ -12,18 +25,22 @@ fn main() {
     // Arg 4: "double quote" (""double quote"")
     // Arg 5: "raw quote" (""raw quote"")
     // ```
+    //
+    // Windows:
+    //  N/A
 
-    Command::new("crates/process/args.sh")
+    #[cfg(unix)]
+    Command::new(&script)
         .arg("nospace")
         .arg("with space")
         .arg("'single quote'")
         .arg("\"double quote\"")
         .arg(r#""raw quote""#)
+        .env("PATH", &paths)
         .spawn()
         .unwrap();
 
-    // Unix:
-    //	Quotes are not preserved, and the script receives the inner value without wrapping quotes.
+    // Unix: Quotes are not preserved, and the script receives the inner value without wrapping quotes.
     //
     // ```
     // Args: nospace with space single quote double quote
@@ -32,9 +49,20 @@ fn main() {
     // Arg 3: single quote ("single quote")
     // Arg 4: double quote ("double quote")
     // ```
-    Command::new("bash")
+    //
+    // Windows: Quotes are not preserved, and the script receives the inner value without wrapping quotes.
+    //
+    // ```
+    // Args: nospace with space single quote double quote
+    // Arg 1: nospace ('nospace')
+    // Arg 2: with space ('with space')
+    // Arg 3: single quote ('single quote')
+    // Arg 4: double quote ('double quote')
+    // ```
+    Command::new(shell)
         .arg("-c")
-        .arg("crates/process/args.sh nospace 'with space' 'single quote' \"double quote\" ")
+        .arg(format!("{script} nospace 'with space' 'single quote' \"double quote\" "))
+        .env("PATH", &paths)
         .spawn()
         .unwrap();
 }

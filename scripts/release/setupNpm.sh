a79bb884db181963a76a7ade0be52075c52566d4
Miles Johnson <mileswjohnson@gmail.com>
2026-01-13 11:28:37 -0800

    build: Enable npm/yarn OIDC.

--- scripts/release/setupNpm.sh
+++ scripts/release/setupNpm.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ -z "${NPM_TOKEN}" ]]; then
-	echo "Missing NPM_TOKEN!" >&2
-else
-	echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
-	echo "npmAuthToken: $NPM_TOKEN" >> ./.yarnrc.yml
-fi
+# if [[ -z "${NPM_TOKEN}" ]]; then
+# 	echo "Missing NPM_TOKEN!" >&2
+# else
+# 	echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+# 	echo "npmAuthToken: $NPM_TOKEN" >> ./.yarnrc.yml
+# fi
 
 # if [[ -d ".yarn/versions" ]]; then
 # 	echo "Yarn versions detected, applying updates"

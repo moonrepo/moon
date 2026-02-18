7cfdb4f4bb75cbc1cf4689b658a29455e7afff37
Miles Johnson <mileswjohnson@gmail.com>
2026-02-17 17:41:24 -0800

    Polish blog post.

--- crates/cli/src/main_exec.rs
+++ crates/cli/src/main_exec.rs
@@ -1,16 +1,17 @@
-mod lookup;
-mod shared;
-
-use starbase::MainResult;
+use proto_shim::exec_command_and_replace;
 use std::env;
 use std::ffi::OsString;
+use std::process::Command;
 
 #[tokio::main]
-async fn main() -> MainResult {
-    shared::run_cli({
-        let mut args = env::args_os().collect::<Vec<_>>();
-        args.insert(1, OsString::from("exec"));
-        args
-    })
-    .await
+async fn main() -> Result<(), std::io::Error> {
+    sigpipe::reset();
+
+    let mut args = env::args_os().collect::<Vec<_>>();
+    args[0] = OsString::from("exec");
+
+    let mut command = Command::new("moon");
+    command.args(args);
+
+    exec_command_and_replace(command)
 }

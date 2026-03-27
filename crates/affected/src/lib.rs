b51670cfc8ee4720c9e469f833340a374212f66d
Miles Johnson <mileswjohnson@gmail.com>
2026-03-26 20:21:46 -0700

    Merge them.

--- crates/affected/src/lib.rs
+++ crates/affected/src/lib.rs
@@ -1,6 +1,8 @@
 mod affected;
 mod affected_tracker;
 mod affected_tracker_async;
+mod project_tracker;
+mod task_tracker;
 
 pub use affected::*;
 pub use affected_tracker::*;
--- crates/config/src/workspace/experiments_config.rs
+++ crates/config/src/workspace/experiments_config.rs
@@ -4,5 +4,8 @@ use schematic::Config;
 config_struct!(
     /// Configures experiments across the entire moon workspace.
     #[derive(Config)]
-    pub struct ExperimentsConfig {}
+    pub struct ExperimentsConfig {
+        /// Track and determine affected projects and tasks asynchronously.
+        pub async_affected_tracking: bool,
+    }
 );

480feaf5abbe3b78ad2670199764381935651c70
Miles Johnson <mileswjohnson@gmail.com>
2026-05-14 09:45:42 -0700

    Move content hash.

--- crates/hash/src/lib.rs
+++ crates/hash/src/lib.rs
@@ -1,6 +1,10 @@
-mod hasher;
+mod content_hash;
+mod content_hasher;
+mod hash_error;
 
-pub use hasher::*;
+pub use content_hash::*;
+pub use content_hasher::*;
+pub use hash_error::*;
 
 #[macro_export]
 macro_rules! hash_fingerprint {

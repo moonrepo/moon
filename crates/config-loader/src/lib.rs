28115f20de23bea055569bb24204679ccf86bad3
Miles Johnson <mileswjohnson@gmail.com>
2026-05-14 21:56:27 -0700

    Use url.

--- crates/config-loader/src/lib.rs
+++ crates/config-loader/src/lib.rs
@@ -7,17 +7,28 @@ mod toolchains_config_ext;
 
 pub use config_loader::*;
 pub use extensions_config_ext::*;
+use moon_common::is_test_env;
 pub use toolchains_config_ext::*;
 
-use proto_core::{PluginLocator, RegistryLocator, warpgate::find_debug_locator};
+use proto_core::{PluginLocator, RegistryLocator, UrlLocator, warpgate::find_debug_locator};
 
 pub fn find_debug_locator_with_fallback(name: &str, version: &str) -> PluginLocator {
     find_debug_locator(name).unwrap_or_else(|| {
-        PluginLocator::Registry(Box::new(RegistryLocator {
-            registry: Some("ghcr.io".into()),
-            namespace: Some("moonrepo".into()),
-            image: name.into(),
-            tag: Some(version.into()),
-        }))
+        // Use URLs within tests, otherwise we hit ghcr.io rate limits when running
+        // tests in CI without authentication. This causes tests to take forever!
+        if is_test_env() {
+            PluginLocator::Url(Box::new(UrlLocator {
+                url: format!(
+                    "https://github.com/moonrepo/plugins/releases/download/{name}-v{version}/{name}.wasm"
+                ),
+            }))
+        } else {
+            PluginLocator::Registry(Box::new(RegistryLocator {
+                registry: Some("ghcr.io".into()),
+                namespace: Some("moonrepo".into()),
+                image: name.into(),
+                tag: Some(version.into()),
+            }))
+        }
     })
 }

0671fe572782e5ab3cf841747818402a26176ab3
Miles Johnson <mileswjohnson@gmail.com>
2026-05-30 14:30:42 -0500

    Debug npm.

--- legacy/core/test-utils/src/cli.rs
+++ legacy/core/test-utils/src/cli.rs
@@ -22,9 +22,9 @@ pub fn create_moon_command_std<T: AsRef<Path>>(path: T) -> std::process::Command
     cmd.env("MOON_TEST", "true");
     cmd.env("STARBASE_TEST", "true");
     // Don't exhaust all cores on the machine
-    cmd.env("MOON_CONCURRENCY", "2");
+    // cmd.env("MOON_CONCURRENCY", "2");
     // Hide install output as it disrupts testing snapshots
-    cmd.env("MOON_TEST_HIDE_INSTALL_OUTPUT", "true");
+    // cmd.env("MOON_TEST_HIDE_INSTALL_OUTPUT", "true");
     // Standardize file system paths for testing snapshots
     cmd.env("MOON_TEST_STANDARDIZE_PATHS", "true");
     // Enable logging for code coverage

247970def14f9323dc7d6f1d2f1cb2267d297e3f
Miles Johnson <mileswjohnson@gmail.com>
2026-05-29 23:33:20 -0500

    Update deps.

--- crates/process/src/helpers.rs
+++ crates/process/src/helpers.rs
@@ -1,10 +1,9 @@
-use cached::proc_macro::cached;
 use moon_common::color;
 use starbase_shell::ShellType;
 use std::path::{Path, PathBuf};
+use std::sync::OnceLock;
 
-#[cached]
-pub fn find_command_on_path(name: String) -> Option<PathBuf> {
+pub fn find_command_on_path(name: &str) -> Option<PathBuf> {
     if name == "pwsh" || name == "powershell" {
         system_env::find_command_on_path("pwsh")
             .or_else(|| system_env::find_command_on_path("powershell"))
@@ -13,9 +12,10 @@ pub fn find_command_on_path(name: String) -> Option<PathBuf> {
     }
 }
 
-#[cached]
 pub fn get_default_shell() -> ShellType {
-    ShellType::detect_with_fallback()
+    static SHELL_CACHE: OnceLock<ShellType> = OnceLock::new();
+
+    *SHELL_CACHE.get_or_init(ShellType::detect_with_fallback)
 }
 
 pub fn format_command_line(command: &str, workspace_root: &Path, working_dir: &Path) -> String {

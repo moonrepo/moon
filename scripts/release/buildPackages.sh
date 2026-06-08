6fb878a44607592c2bf3bd152247a051b48ee240
Miles Johnson <mileswjohnson@gmail.com>
2026-06-07 22:38:50 -0700

    Fix script.

--- scripts/release/buildPackages.sh
+++ scripts/release/buildPackages.sh
@@ -2,12 +2,4 @@
 
 export NODE_ENV=production
 
-# Build types first since everything depends on it
-vp pack packages/types
-
-# Then just build everything
-vp pack
-
-# Then build the visualizer with vite
-yarn workspace @moonrepo/visualizer run build
-yarn workspace website run build
+vp run -r build

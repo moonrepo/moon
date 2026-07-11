102cde1d0ba98364c74753db24a0e570fbc8ac8e
Miles Johnson <mileswjohnson@gmail.com>
2026-07-11 16:21:32 -0700

    Dont bubble error.

--- crates/action-pipeline/src/action_pipeline.rs
+++ crates/action-pipeline/src/action_pipeline.rs
@@ -176,13 +176,11 @@ impl ActionPipeline {
 
         let process_registry = ProcessRegistry::instance();
         let mut actions = vec![];
-        let mut error = None;
 
-        while let Some(mut action) = receiver.recv().await {
+        while let Some(action) = receiver.recv().await {
             if self.bail && action.should_bail() || action.should_abort() {
                 process_registry.terminate_running();
                 abort_token.cancel();
-                error = Some(action.get_error());
             }
 
             actions.push(action);
@@ -238,10 +236,6 @@ impl ActionPipeline {
 
         self.actions = actions;
         self.duration = Some(start.elapsed());
-
-        if let Some(error) = error {
-            return Err(error);
-        }
 
         Ok(())
     }

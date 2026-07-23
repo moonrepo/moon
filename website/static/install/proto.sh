a7fd88f5830381568ff70dc7c0e0b50abf372a9d
Miles Johnson <mileswjohnson@gmail.com>
2026-07-23 10:01:45 -0700

    Update installers.

--- website/static/install/proto.sh
+++ website/static/install/proto.sh
@@ -88,15 +88,17 @@ else
 	download_url="https://github.com/moonrepo/proto/releases/download/v$version/$target$ext"
 fi
 
-temp_dir="$HOME/.proto/temp/proto/$target"
-download_file="$temp_dir$ext"
-
-if [[ -z "$PROTO_HOME" ]]; then
-	install_dir="$HOME/.proto/bin"
+if [[ -n "$PROTO_HOME" ]]; then
+	home_dir="$PROTO_HOME"
+elif [[ -n "$XDG_DATA_HOME" ]]; then
+	home_dir="$XDG_DATA_HOME/proto"
 else
-	install_dir="$PROTO_HOME/bin"
+	home_dir="$HOME/.proto"
 fi
 
+temp_dir="$home_dir/temp/proto/$target"
+download_file="$temp_dir$ext"
+install_dir="$home_dir/bin"
 bin_path="$install_dir/$bin"
 shim_path="$install_dir/$shim_bin"
 

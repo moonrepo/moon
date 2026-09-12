209fae43ca572f36afd86f35ce91f2412f7e9914
Miles Johnson <mileswjohnson@gmail.com>
2026-09-12 12:29:48 -0700

    Update schemas.

--- crates/config/src/shapes/input.rs
+++ crates/config/src/shapes/input.rs
@@ -137,7 +137,7 @@ config_struct!(
         pub glob: GlobPath,
 
         /// Cache the glob walking result for increased performance.
-        #[serde(skip_serializing_if = "is_false")]
+        #[serde(default = "default_true", skip_serializing_if = "is_false")]
         #[setting(default = true)]
         pub cache: bool,
     }
